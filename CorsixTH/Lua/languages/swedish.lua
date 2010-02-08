d545ef7880d5c18cabd610e8c529c065243fc43e
Edvin Linge <edvin.linge@gmail.com>
2010-02-08 20:48:00 +0000

    Added new strings to the swedish lua file.

--- CorsixTH/Lua/languages/swedish.lua
+++ CorsixTH/Lua/languages/swedish.lua
@@ -23,7 +23,35 @@ Inherit("english")
 Inherit("original_strings", 5)
 
 -- override
--- TODO? Any original strings that are off in swedish translation?
+object.reception_desk = "Reception"
+-- TODO? Any more original strings that are off in swedish translation?
 
 -- new strings
--- TODO
+menu_options_game_speed.pause = "  PAUSA"
+
+menu_debug = {
+  transparent_walls           = utf8 "  TRANSPARENTA VÄGGAR",
+  limit_camera                = utf8 "  BEGRÄNSA KAMERAN",
+  disable_salary_raise        = utf8 "  STÄNG AV LÖNEÖKNINGAR",
+  make_debug_patient          = "  SKAPA DEBUGPATIENT",
+  spawn_patient               = "  GENERERA VANLIG PATIENT",
+  make_adviser_talk           = utf8 "  LÅT RÅDGIVAREN PRATA",
+  show_watch                  = "  VISA KLOCKA",
+  place_objects               = "  PLACERA OBJEKT",
+  map_overlay                 = utf8 "  KARTÖVERSIKT",
+  sprite_viewer               = "  GRAFIKVISARE",
+}
+menu_debug_overlay = {
+  none                        = "  INGET",
+  flags                       = "  FLAGGOR",
+  byte_0_1                    = "  BYTE 0 & 1",
+  byte_floor                  = "  BYTE GOLV",
+  byte_n_wall                 = utf8 "  BYTE N VÄGG",
+  byte_w_wall                 = utf8 "  BYTE W VÄGG",
+  byte_5                      = "  BYTE 5",
+  byte_6                      = "  BYTE 6",
+  byte_7                      = "  BYTE 7",
+  parcel                      = "  TOMT",
+}
+
+adviser.room_forbidden_non_reachable_parts = utf8 "Rummet kan inte placeras här eftersom delar av sjukhuset då blir oåtkomliga."
