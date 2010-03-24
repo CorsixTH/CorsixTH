d4c3aea52231ff38a30385d4e54d52a1fcff3f5c
Edvin Linge <edvin.linge@gmail.com>
2010-03-24 12:15:57 +0000

    Applied Revision 591 to the beta 2 branch.

--- CorsixTH/Lua/languages/french.lua
+++ CorsixTH/Lua/languages/french.lua
@@ -22,11 +22,6 @@ Language("french" , "fr", "fre", "fra")
 Inherit("english")
 Inherit("original_strings", 1)
 
--- override
-adviser.warnings.money_low = "Vous n'avez presque plus d'argent !" -- Funny. Exists in German translation, but not existent in english?
--- TODO: tooltip.graphs.reputation -- this tooltip talks about hospital value. Actually it should say reputation.
--- TODO: tooltip.status.close -- it's called status window, not overview window.
-
 -- new strings
 object.litter = "Déchet"
 
@@ -43,7 +38,7 @@ menu_debug = {
   show_watch                  = "  AFFICHER LE COMPTE A REBOUR  ",
   create_emergency            = "  CREER UNE URGENCE  ",
   place_objects               = "  PLACER DES OBJETS  ",
-  dump_strings                = "  DUMP STRINGS  ",
+  dump_strings                = "  EXTRAIRE LES TEXTES  ",
   map_overlay                 = "  MAP OVERLAY  ",
   sprite_viewer               = "  VISIONNEUSE DE SPRITES  ",
 }
@@ -85,13 +80,13 @@ fax = {
   tutorial = {
     utf8 "Bienvenue dans votre premier hôpital!",
     "Souhaitez-vous un petit tutoriel ?",
-    "Oui, montre-moi les bases STP.",
+    "Oui, montrez-moi les bases SVP.",
     utf8 "Non, je sais déjà comment faire.",
   },
 }
 
-misc.not_yet_implemented = "(pas encore implémenté)"
-misc.no_heliport = "Aucune maladie n'a été découverte pour l'instant, ou il n'y a pas d'héliport sur cette carte."
+misc.not_yet_implemented = utf8 "(pas encore implémenté)"
+misc.no_heliport = utf8 "Aucune maladie n'a été découverte pour l'instant, ou il n'y a pas d'héliport sur cette carte."
 
 main_menu = {
   new_game = "Nouvelle Partie",
@@ -125,4 +120,26 @@ errors = {
   load_prefix = "Erreur lors du chargement de la partie: ",
 }
 
+totd_window = {
+  tips = {
+    utf8 "Chaque hôpital a besoin d'un bureau de réception et d'un bureau de généraliste. Après, tout dépend du type de patients qui visitent votre hôpital. Une pharmacie est toujours un bon choix malgré tout.",
+    utf8 "Les machines telles que le Gonflage ont besoin de maintenance. Embauchez un ou deux agents de maintenance pour réparer vos machines, ou vous risquerez d'avoir des blessés parmi le personnel ou les patients.",
+    utf8 "Après un certain temps, vos employés seront fatigués. Pensez à construire une salle de repos où ils pourront se détendre.",
+    utf8 "Placez suffisamment de radiateurs pour garder vos employés et patients au chaud, ou ils deviendront mécontents.",
+    utf8 "Le niveau de compétence d'un docteur influence beaucoup la qualité et la rapidité de ses diagnostics. Utilisez un médecin expérimenté comme généraliste et vous n'aurez plus besoin d'autant de salles de diagnostics.",
+    utf8 "Les internes et les médecins peuvent augmenter leurs compétences auprès d'un consultant dans la salle de formation. Si le consultant a des qualifications pariculières (chirurgien, psyschiatre ou chercheur), il transférera ses connaissances à ses élèves.",
+    utf8 "Avez-vous essayé d'entrer le numéro d'urgence Européen (112) dans le fax ? Vérifiez que vous avez du son !",
+    utf8 "Le menu d'options n'est pas encore implémenté, mais vous pouvez ajuster les paramètres tels que la résolution ou la langue en éditant le fichier config.txt dans le dossier du jeu.",
+    utf8 "Vous avez choisi une autre langue que l'anglais, mais il y du texte en anglais partout ? Aidez-nous à traduire les textes manquants dans votre langue!",
+    utf8 "L'équipe de CorsixTH cherche du renfort ! Vous êtes intéressé par coder, traduire ou faire des graphismes pour CorsixTH ? Contactez-nous sur notre Forum, Liste de Diffusion ou Canal IRC (corsix-th sur quakenet).",
+    utf8 "Si vous avez trouvé un bug, SVP, reportez le sur notre gestionnaire de bugs: th-issues.corsix.org.",
+    utf8 "Le saviez-vous ? CorsixTH a été rendu public pour la première fois le 24 juillet 2009. La première publication a été la beta 1 jouable le 24 décembre 2009. Après trois mois de plus, nous sommes fiers de vous présenter la beta 2 (publiée le 24 mars 2010).",
+  },
+  previous = utf8 "Astuce Précédente",
+  next = "Astuce Suivante",
+}
 
+tooltip.totd_window = {
+  previous = utf8 "Affiche l'astuce précédente",
+  next = "Affiche l'astuce suivante",
+}
