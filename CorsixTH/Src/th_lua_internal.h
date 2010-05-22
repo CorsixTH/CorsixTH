773fa0b910331b9f4959d7c7b00e3ae61405b162
Peter Cawley <corsix@gmail.com>
2010-05-22 08:53:29 +0000

    Fix small (but critical) mistake in r694.

--- CorsixTH/Src/th_lua_internal.h
+++ CorsixTH/Src/th_lua_internal.h
@@ -74,7 +74,7 @@ void luaT_setclosure(const THLuaRegisterState_t *pState, lua_CFunction fn,
     lua_pushvalue(pState->L, iCurrentClassMT); \
     lua_replace(pState->L, luaT_environindex); \
     /* Set the __gc metamethod to C++ destructor */ \
-    luaT_pushcclosure(pState->L, luaT_stdgc<typnam, luaT_environindex>, 1); \
+    luaT_pushcclosure(pState->L, luaT_stdgc<typnam, luaT_environindex>, 0); \
     lua_setfield(pState->L, iCurrentClassMT, "__gc"); \
     /* Set the depersist size */ \
     lua_pushinteger(pState->L, sizeof(typnam)); \
