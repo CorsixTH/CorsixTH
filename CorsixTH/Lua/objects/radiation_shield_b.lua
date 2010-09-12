aad315e0af28a9827b9a3bd895f07f0d965390e7
Edvin Linge <edvin.linge@gmail.com>
2010-09-12 14:51:50 +0000

    Added: Radiation shield to the console in the X-ray room, from issue 410. (Thanks WendellMisiedjan!)

--- CorsixTH/Lua/objects/radiation_shield_b.lua
+++ CorsixTH/Lua/objects/radiation_shield_b.lua
@@ -1,8 +1,5 @@
 --[[ Copyright (c) 2010 Wendell Misiedjan
-
-Edited by: Wendell Misiedjan
-
-Based on: Manuel König files.
+Edited with some help of Corsix.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -23,54 +20,34 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. --]]
 
 local object = {}
-object.id = "radiation_shield"
-object.slave_id = "radiation_shield_b"
+object.id = "radiation_shield_b"
 object.class = "RadiationShield"
 object.thob = 28
 object.name = _S.object.radiation_shield
 object.tooltip = _S.tooltip.objects.radiation_shield
-object.ticks = true
-object.build_cost = 5000
-object.build_preview_animation = 922
+object.ticks = false
+object.build_cost = 0
 
-local function copy_north_to_south(t)
-  t.south = t.north
+local function copy_east_to_west(t)
+  t.west = t.east
   return t
 end
 
-object.idle_animations = copy_north_to_south {
-  north = 794,
+object.idle_animations = copy_east_to_west {
+  east = 1968,
 }
 
-object.usage_animations = copy_north_to_south {
-  north = {
-    begin_use   = {Doctor = 798}, -- Doctor sits down
-    begin_use_2 = {Doctor = 806}, -- Pulls the handle
-    in_use      = {Doctor = {
-      810, -- Sits idle
-      814, -- Pushing buttons
-    }},
-    finish_use  = {Doctor = 802}, -- Doctor stands up
-  },
-}
 object.orientations = {
   north = {
-    render_attach_position = {0, -1},
-    footprint = { {-2, -1}, {-1, -1}, {0, -1},
-                  {-2, 0}, {-1, 0}, {0, 0, only_passable = true}, 
-				  {-2, 1}, {-1, 1}, {0, 1}},
-    use_position = {0, 0}
+    render_attach_position = {0, 2},
+    use_position = {0, 0},
+    footprint = {},
   },
   east = {
-    render_attach_position = {0, -1},
-    footprint = { {-1, -2}, {0, -2},{1, -2},
-                  {-1, -1}, {0, -1},{1, -1},
-                  {-1, 0}, {0, 0, only_passable = true},{1, 0} },
-    use_position = {0, 0}
+    render_attach_position = {1, 1},
+    use_position = {0, 0},
+    footprint = {},
   },
 }
-
-class "RadiationShield" (Object)
-RadiationShield:slaveMixinClass()
 
 return object
