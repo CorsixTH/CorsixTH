8b5f53b0992513c321a46deb3eff6256b0bd6ef6
Peter Cawley <corsix@gmail.com>
2010-12-08 21:01:06 +0000

    MapEdit now relies of FindwxWidgets from CMake 2.8.3 rather than a bundled finder - this is more robust and fixes compilation problems in Linux, at the cost of requiring CMake 2.8.3 (or later) to compile MapEdit. Fix spurious code at the top of block_gallery.cpp.

--- MapEdit/Src/block_gallery.cpp
+++ MapEdit/Src/block_gallery.cpp
@@ -20,20 +20,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#pragma once
-// For compilers that support precompilation, includes "wx/wx.h".
-#include "wx/wxprec.h"
-
-#ifdef __BORLANDC__
-    #pragma hdrstop
-#endif
-
-// for all others, include the necessary headers (this file is usually all you
-// need because it includes almost all "standard" wxWidgets headers)
-#ifndef WX_PRECOMP
-    #include "wx/wx.h"
-#endif
-// ----------------------------
 #include "block_gallery.h"
 #include <map>
 
