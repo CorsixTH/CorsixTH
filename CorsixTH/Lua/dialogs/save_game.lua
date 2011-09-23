9142911a40d3531a881f2e7d1200828ac17473b0
Edvin Linge <edvin.linge@gmail.com>
2011-09-23 06:42:38 +0000

    Fix: Make sure that the load and save dialogs always have their dependent file available.

--- CorsixTH/Lua/dialogs/save_game.lua
+++ CorsixTH/Lua/dialogs/save_game.lua
@@ -18,7 +18,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. --]]
 
-dofile("dialogs/menu_list_dialog")
+dofile("dialogs/file_browser")
 
 --! Save Game Window
 class "UISaveGame" (UIFileBrowser)
