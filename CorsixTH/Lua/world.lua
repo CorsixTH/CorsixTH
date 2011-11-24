79ae9e1084887274d557dc8bb50dc4cd109347c4
Ola Skogrand <grimdoc@gmail.com>
2011-11-24 02:45:58 +0000

    Adds the functionality so the queue size can be set to zero on room - and no patients who isn't in the queue will enter (you can prevent patients from entering rooms).

--- CorsixTH/Lua/world.lua
+++ CorsixTH/Lua/world.lua
@@ -1415,7 +1415,7 @@ function World:findRoomNear(humanoid, room_type_id, distance, mode)
     distance = 2^30
   end
   for _, r in pairs(self.rooms) do repeat
-    if r.built and (not room_type_id or r.room_info.id == room_type_id) and r.is_active then
+    if r.built and (not room_type_id or r.room_info.id == room_type_id) and r.is_active and r.door.queue.max_size ~= 0 then
       local x, y = r:getEntranceXY(false)
       local d = self:getPathDistance(humanoid.tile_x, humanoid.tile_y, x, y)
       if d > distance then
