8e709b05681b9dd62e153ce6212c4763d793fa97
Edvin Linge <edvin.linge@gmail.com>
2012-10-29 21:06:46 +0000

    Prepare for 0.11 branch

--- CorsixTH/Lua/app.lua
+++ CorsixTH/Lua/app.lua
@@ -967,8 +967,12 @@ end
 -- a specific savegame verion is from.
 function App:getVersion(version)
   local ver = version or self.savegame_version
-  if ver > 51 then
+  if ver > 54 then
     return "Trunk"
+  elseif ver > 53 then
+    return "0.11"
+  elseif ver > 51 then
+    return "0.10"
   elseif ver > 45 then
     return "0.01"
   else
