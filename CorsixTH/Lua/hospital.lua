a7eb0a8ab07e99f15f5ab1fa577e053fbaa7695a
Edvin Linge <edvin.linge@gmail.com>
2012-11-01 16:43:40 +0000

    The tabs were somehow readded after I removed them...

--- CorsixTH/Lua/hospital.lua
+++ CorsixTH/Lua/hospital.lua
@@ -562,13 +562,13 @@ function Hospital:afterLoad(old, new)
   
   if old < 54 then
     local current = self.research.research_policy.specialisation.current
-	if current and not current.dummy and not current.thob and not current.drug then
-	  for _, diseaseEntry in pairs(self.disease_casebook) do
+      if current and not current.dummy and not current.thob and not current.drug then
+        for _, diseaseEntry in pairs(self.disease_casebook) do
         if diseaseEntry.concentrate_research then
-		  self.research:concentrateResearch(diseaseEntry.disease.id)
-		  self.research:concentrateResearch(diseaseEntry.disease.id)
-		end
-	  end
+          self.research:concentrateResearch(diseaseEntry.disease.id)
+          self.research:concentrateResearch(diseaseEntry.disease.id)
+        end
+      end
     end
   end  
 end
