05c958fbc6ef55303e5a9f70a425a2ceecce6e78
benckx <benoit.vleminckx@gmail.com>
2014-09-26 15:22:21 +0200

    some patients now leave without paying for treatment

--- CorsixTH/Lua/world.lua
+++ CorsixTH/Lua/world.lua
@@ -2378,6 +2378,13 @@ function World:afterLoad(old, new)
   if old < 80 then
     self:determineWinningConditions()
   end
+  if old < 86 then
+    -- Added these two hospital variables, related to price distortion
+    for _, hospital in ipairs(self.hospitals) do
+      hospital.under_priced_threshold = -0.3
+      hospital.over_priced_threshold = 0.3
+    end
+  end
 
   self.savegame_version = new
 end
