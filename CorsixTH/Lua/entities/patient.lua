CorsixTH/CorsixTH

commit b54c5a832baa5815511ab7f66f78351b0c4c338c

Replaced setting nil hospital with despawn function

CorsixTH/Lua/entities/patient.lua
@@ -487,11 +487,11 @@ end
 
 function Patient:goHome(cured)
   local hosp = self.hospital
-  if not hosp and self.going_home then
+  if self.going_home then
     -- The patient should be going home already! Anything related to the hospital
     -- will not be updated correctly, but we still want to try to get the patient to go home.
     TheApp.world:gameLog("Warning: goHome called when the patient is already going home")
-    self:setHospital(nil)
+    self:despawn()
     return
   end
   if not cured then
@@ -525,7 +525,7 @@ function Patient:goHome(cured)
   if room then
     room:makeHumanoidLeave(self)
   end
-  self:setHospital(nil)
+  self:despawn()
 end
 
 -- This function handles changing of the different attributes of the patient.

CorsixTH/Lua/entities/staff.lua
@@ -371,7 +371,7 @@ function Staff:fire()
   self:setDynamicInfoText(_S.dynamic_info.staff.actions.fired)
   self.fired = true
   self.hospital:changeReputation("kicked")
-  self:setHospital(nil)
+  self:despawn()
   self.hover_cursor = nil
   self.attributes["fatigue"] = nil
   self:leaveAnnounce()
@@ -385,7 +385,7 @@ function Staff:fire()
 end
 
 function Staff:die()
-  self:setHospital(nil)
+  self:despawn()
   if self.task then
     -- If the staff member had a task outstanding, unassigning them from that task.
     -- Tasks with no handyman assigned will be eligible for reassignment by the hospital.

CorsixTH/Lua/entities/humanoid.lua
@@ -426,19 +426,24 @@ end
 -- Set the `Hospital` which is responsible for treating or employing the
 -- `Humanoid`. In single player games, this has little effect, but it is very
 -- important in multiplayer games.
---!param hospital (Hospital, nil) The `Hospital` which should be responsible
--- for the `Humanoid`. If nil, then the `Humanoid` is despawned.
+--!param hospital (Hospital) The `Hospital` which should be responsible
+-- for the `Humanoid`.
 function Humanoid:setHospital(hospital)
   self.hospital = hospital
-  if not hospital or not hospital.is_in_world then
-    local spawn_points = self.world.spawn_points
-    self:setNextAction{
-      name = "spawn",
-      mode = "despawn",
-      point = spawn_points[math.random(1, #spawn_points)],
-      must_happen = true,
-    }
+  if not hospital.is_in_world then
+    self:despawn()
   end
+end
+
+--! Despawn the humanoid.
+function Humanoid:despawn()
+  local spawn_points = self.world.spawn_points
+  self:setNextAction{
+    name = "spawn",
+    mode = "despawn",
+    point = spawn_points[math.random(1, #spawn_points)],
+    must_happen = true,
+  }
 end
 
 -- Function to activate/deactivate moods of a humanoid.

CorsixTH/Lua/entities/inspector.lua
@@ -45,7 +45,7 @@ function Inspector:goHome()
 
   self:unregisterCallbacks()
   self.going_home = true
-  self:setHospital(nil)
+  self:despawn()
 end
 
 --[[ Called when the inspector has left the map ]]

CorsixTH/Lua/entities/vip.lua
@@ -119,7 +119,7 @@ function Vip:goHome()
   self.going_home = true
   -- save self.hospital so we can reference it in self:onDestroy
   self.last_hospital = self.hospital
-  self:setHospital(nil)
+  self:despawn()
 end
 
 -- called when the vip is out of the hospital grounds

CorsixTH/Lua/humanoid_actions/die.lua
@@ -54,7 +54,7 @@ local action_die_tick; action_die_tick = permanent"action_die_tick"( function(hu
     humanoid:setAnimation(humanoid.die_anims.fly_east, mirror)
     humanoid:setTilePositionSpeed(humanoid.tile_x, humanoid.tile_y, nil, nil, 0, -4)
   else
-    humanoid:setHospital(nil)
+    humanoid:despawn()
     humanoid.world:destroyEntity(humanoid)
   end
 end)

CorsixTH/Lua/humanoid_actions/spawn.lua
@@ -27,7 +27,7 @@ local orient_opposite = {
 
 local action_spawn_despawn = permanent"action_spawn_despawn"( function(humanoid)
   if humanoid.hospital then
-    humanoid:setHospital(nil)
+    humanoid:despawn()
   end
   humanoid.world:destroyEntity(humanoid)
 end)

CorsixTH/Lua/humanoid_actions/use_object.lua
@@ -319,7 +319,7 @@ action_use_object_tick = permanent"action_use_object_tick"( function(humanoid)
       end
 
       if action.destroy_user_after_use then
-        humanoid:setHospital(nil)
+        humanoid:despawn()
         humanoid.world:destroyEntity(humanoid)
       else
         humanoid:finishAction(action)

commit cbf95d369b9665c668f237f707410f66b1f492b0

Fixes regression caused by commit b54c5a8.
- Override despawn to remove entities from hospital - Fixes #1031

CorsixTH/Lua/entities/patient.lua
@@ -207,17 +207,19 @@ function Patient:completeDiagnosticStep(room)
   self:modifyDiagnosisProgress(diagnosis_base + (diagnosis_bonus * multiplier))
 end
 
+-- Sets the hospital for the patient - additionally removing them from a
+-- hospital if they already belong to one. For player hospitals, patients who
+-- are not debug or emergency patients are made to seek a reception desk.
+--!param hospital (Hospital): hospital to assign to patient
 function Patient:setHospital(hospital)
   if self.hospital then
     self.hospital:removePatient(self)
   end
   Humanoid.setHospital(self, hospital)
-  if hospital then
-    if hospital.is_in_world and not self.is_debug and not self.is_emergency then
-      self:setNextAction{name = "seek_reception"}
-    end
-    hospital:addPatient(self)
+  if hospital.is_in_world and not self.is_debug and not self.is_emergency then
+    self:setNextAction{name = "seek_reception"}
   end
+  hospital:addPatient(self)
 end
 
 --! Decide the ID of the disease or treatment that the patient is paying for.
@@ -631,6 +633,12 @@ function Patient:goHome(reason, disease_id)
     room:makeHumanoidLeave(self)
   end
   self:despawn()
+end
+
+-- Despawns the patient and removes them from the hospital
+function Patient:despawn()
+  self.hospital:removePatient(self)
+  Humanoid.despawn(self)
 end
 
 -- This function handles changing of the different attributes of the patient.

CorsixTH/Lua/entities/staff.lua
@@ -404,6 +404,12 @@ function Staff:die()
   self.world.ui.hospital:humanoidDeath(self)
 end
 
+-- Despawns the staff member and removes them from the hospital
+function Staff:despawn()
+  self.hospital:removeStaff(self)
+  Humanoid.despawn(self)
+end
+
 -- Function which is called when the user clicks on the staff member.
 -- Responsible for opening a staff information dialog on left click and picking
 -- up the staff member on right click.
@@ -672,10 +678,9 @@ function Staff:onPlaceInCorridor()
   end
 end
 
+-- Sets the Hospital for a member of staff
+--!param hospital (Hospital) - hospital to assign to member of staff
 function Staff:setHospital(hospital)
-  if self.hospital then
-    self.hospital:removeStaff(self)
-  end
   Humanoid.setHospital(self, hospital)
   self:updateDynamicInfo()
 end