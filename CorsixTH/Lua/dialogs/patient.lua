970ecb7066d82e4f1e7c3a9ef37d094ac2d048de
Alberth <alberth289346@gmail.com>
2016-09-21 20:08:45 +0200

    Draw patient health bars in a separate function.

--- CorsixTH/Lua/dialogs/patient.lua
+++ CorsixTH/Lua/dialogs/patient.lua
@@ -114,6 +114,20 @@ function UIPatient.normaliseWarmth(warmth)
   return warmth
 end
 
+--! Draw a bar in the patient dialogue window.
+--!param canvas Canvas to draw at.
+--!param xbase Horizontal base position.
+--!param ybase Vertical base position.
+--!param xpos Horizontal offset.
+--!param ypos Vertical offset.
+--!param value Fraction to draw.
+function UIPatient:drawBar(canvas, xbase, ybase, xpos, ypos, value)
+  local width = math.floor(value * 40 + 0.5)
+  for dx = 0, width - 1 do
+    self.panel_sprites:draw(canvas, xbase, xpos + 58 + dx, ypos + ybase)
+  end
+end
+
 function UIPatient:draw(canvas, x_, y_)
   local x, y = self.x + x_, self.y + y_
   local map = self.ui.app.map
@@ -141,35 +155,12 @@ function UIPatient:draw(canvas, x_, y_)
   self.ui.app.map:draw(canvas, px, py, 75, 76, x + 17, y + 216)
   Window.draw(self, canvas, x_, y_)
 
-  -- The patients happiness. Each bar is by default half way if the actual value
-  -- cannot be found.
-  local happiness_bar_width = 22
-  if patient.attributes["happiness"] then
-    happiness_bar_width = math_floor(patient.attributes["happiness"] * 40 + 0.5)
-  end
-  if happiness_bar_width ~= 0 then
-    for dx = 0, happiness_bar_width - 1 do
-      self.panel_sprites:draw(canvas, 348, x + 58 + dx, y + 126)
-    end
-  end
-  -- The patients thirst level
-  local thirst_bar_width = 22
-  if patient.attributes["thirst"] then
-    thirst_bar_width = math_floor((1 - patient.attributes["thirst"]) * 40 + 0.5)
-  end
-  if thirst_bar_width ~= 0 then
-    for dx = 0, thirst_bar_width - 1 do
-      self.panel_sprites:draw(canvas, 351, x + 58 + dx, y + 154)
-    end
-  end
-  -- How warm the patient feels
+  -- The patient bars (happiness, thirst, and warmth).
   local warmth = self.normaliseWarmth(patient.attributes["warmth"])
-  local warmth_bar_width = math_floor(warmth * 40 + 0.5)
-  if warmth_bar_width ~= 0 then
-    for dx = 0, warmth_bar_width - 1 do
-      self.panel_sprites:draw(canvas, 349, x + 58 + dx, y + 183)
-    end
-  end
+
+  self.drawBar(canvas, 348, 126, x, y, patient.attributes["happiness"] or 0.5)
+  self.drawBar(canvas, 351, 154, x, y, 1 - (patient.attributes["thirst"] or 0.5))
+  self.drawBar(canvas, 349, 183, x, y, warmth)
 
   if self.history_panel.visible then
     self:drawTreatmentHistory(canvas, x + 40, y + 25)
