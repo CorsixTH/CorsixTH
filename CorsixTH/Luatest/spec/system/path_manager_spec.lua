e3610470ec6781b48f4dcb1b5536680f6f21538e
Pavel Schoffer <xschof00@gmail.com>
2018-01-09 21:02:07 +0100

    Adds Path Manager

---
+++ CorsixTH/Luatest/spec/system/path_manager_spec.lua
@@ -0,0 +1,32 @@
+--[[ Copyright (c) 2018 Pavel "sofo" Schoffer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. --]]
+
+require("class_test_base")
+
+require("system.path_manager")
+
+describe("Path Manager -", function()
+    it("should return right separator", function()
+        local path_manager = PathManager()
+        local expected_separator = package.config:sub(1, 1)
+        assert.equal(expected_separator, path_manager:getSeparator())
+    end)
+end)
+  
