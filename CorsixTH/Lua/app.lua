25b4424f65c894a3dd846db4e0722c0dae4932bf
Stephen E. Baker <baker.stephen.e@gmail.com>
2018-07-11 19:58:08 -0400

    Bump version to v0.62-rc2

--- CorsixTH/Lua/app.lua
+++ CorsixTH/Lua/app.lua
@@ -28,7 +28,7 @@ local runDebugger = corsixth.require("run_debugger")
 -- Increment each time a savegame break would occur
 -- and add compatibility code in afterLoad functions
 
-local SAVEGAME_VERSION = 125
+local SAVEGAME_VERSION = 126
 
 class "App"
 
@@ -1301,10 +1301,10 @@ end
 -- a specific savegame verion is from.
 function App:getVersion(version)
   local ver = version or self.savegame_version
-  if ver > 125 then
+  if ver > 126 then
     return "Trunk"
   elseif ver > 122 then
-    return "v0.62-rc1"
+    return "v0.62-rc2"
   elseif ver > 111 then
     return "v0.61"
   elseif ver > 105 then
