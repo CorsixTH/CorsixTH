13319041c2e75fc59642966ce8d17a1bd10d6834
Alberth <alberth289346@gmail.com>
2018-07-31 16:43:52 +0200

    Remove BOM from norwegian trnalsation

--- CorsixTH/Lua/languages/norwegian.lua
+++ CorsixTH/Lua/languages/norwegian.lua
@@ -1,4 +1,4 @@
-﻿--[[ Copyright (c) 2017 Erlend Mongstad, Ola Skogrand
+--[[ Copyright (c) 2017 Erlend Mongstad, Ola Skogrand
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
