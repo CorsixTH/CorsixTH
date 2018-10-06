37d5257aac29ea502f3aae6548d2dca5ae4632a5
Justin <justin.mugford@gmail.com>
2018-10-07 09:38:15 +1000

    Autospy can be discovered multiple times - morale related chances

--- CorsixTH/Lua/app.lua
+++ CorsixTH/Lua/app.lua
@@ -28,7 +28,7 @@ local runDebugger = corsixth.require("run_debugger")
 -- Increment each time a savegame break would occur
 -- and add compatibility code in afterLoad functions
 
-local SAVEGAME_VERSION = 130
+local SAVEGAME_VERSION = 131
 
 class "App"
 
