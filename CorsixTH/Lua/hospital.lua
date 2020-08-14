ed805fd9f83631976f71f58daa9b814b9d99dddb
Toby Lane <mrtobylane@gmail.com>
2020-08-14 18:46:25 +0100

    Load initial reputation from level config if set

--- CorsixTH/Lua/hospital.lua
+++ CorsixTH/Lua/hospital.lua
@@ -33,17 +33,21 @@ function Hospital:Hospital(world, avail_rooms, name)
   local level = world.map.level_number
   local balance = 40000
   local interest_rate = 0.01
+  local reputation_min = 0
+  local reputation_max = 1000
+  local reputation = math.round((reputation_min + reputation_max) / 2)
   if level_config then
     if level_config.towns and level_config.towns[level] then
       balance = level_config.towns[level].StartCash
       interest_rate = level_config.towns[level].InterestRate / 10000
+      reputation = level_config.towns[level].StartRep or reputation
     elseif level_config.town then
       balance = level_config.town.StartCash
       interest_rate = level_config.town.InterestRate / 10000
+      reputation = level_config.town.StartRep or reputation
     end
   end
   self.name = name or "PLAYER"
-  -- TODO: Variate initial reputation etc based on level
   -- When playing in free build mode you don't care about money.
   self.balance = not world.free_build_mode and balance or 0
   self.loan = 0
@@ -82,9 +86,9 @@ function Hospital:Hospital(world, avail_rooms, name)
   self.inflation_rate = 0.045
   self.salary_incr = level_config.gbv.ScoreMaxInc or 300
   self.sal_min = level_config.gbv.ScoreMaxInc / 6 or 50
-  self.reputation = 500
-  self.reputation_min = 0
-  self.reputation_max = 1000
+  self.reputation = reputation
+  self.reputation_min = reputation_min
+  self.reputation_max = reputation_max
 
   local difficulty = self.world.map:getDifficulty()
   -- Price distortion level under which the patients might consider the
@@ -140,7 +144,7 @@ function Hospital:Hospital(world, avail_rooms, name)
       visitors = 0,
       cures = 0,
       deaths = 0,
-      reputation = 500, -- TODO: Always 500 from the beginning?
+      reputation = reputation
     }
   }
   self.money_in = 0
