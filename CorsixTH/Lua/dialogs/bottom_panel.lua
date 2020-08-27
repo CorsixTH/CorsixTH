247c0fcc5fd5ad944ec03e22010aecb4d3cc2ffa
lewri <lewri@users.noreply.github.com>
2020-08-27 20:12:15 +0100

    Update bottom_panel.lua

--- CorsixTH/Lua/dialogs/bottom_panel.lua
+++ CorsixTH/Lua/dialogs/bottom_panel.lua
@@ -863,7 +863,7 @@ function UIBottomPanel:afterLoad(old, new)
     end
     self.bank_button = self.buttons[1]:makeToggle()
   end
-  -- Hotfix to force re-calculation of the money font (must be removed in future, see issue #1193)
+  -- Hotfix to force re-calculation of the money font (see issue #1193)
   self.money_font = self.ui.app.gfx:loadFont("QData", "Font05V")
 
   self:registerKeyHandlers()
