74cf95239b4efdfa88d551d8ba821dace3ba802c
lewri <lewri@users.noreply.github.com>
2020-09-08 18:34:02 +0100

    Travis fix

--- CorsixTH/Lua/dialogs/resizables/cheats_dialog.lua
+++ CorsixTH/Lua/dialogs/resizables/cheats_dialog.lua
@@ -19,10 +19,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. --]]
 
-corsixth.require("announcer")
-
-local AnnouncementPriority = _G["AnnouncementPriority"]
-
 --! A dialog for activating cheats
 class "UICheats" (UIResizable)
 
