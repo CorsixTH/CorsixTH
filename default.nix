e3811a2d220929738efd5e972ba9bb39ffd4bb12
alexandre-lavoie <alexandre.lavoie00@gmail.com>
2021-05-16 03:43:18 -0400

    Most of Nixpkgs support
    Added a `default.nix` that exposes most CMakeLists options.
    
    Mostly supported on `NixOS` (not tested on other distros).
    
    Issue with `timidity` - appears to fail to find file?
    
    Start of implementation of `buildAnimView`.

---
+++ default.nix
@@ -0,0 +1,86 @@
+{
+    pkgs ? (
+        let 
+            hostPkgs = import <nixpkgs> {};
+            pinnedPkgs = hostPkgs.fetchFromGitHub {
+            owner = "NixOS";
+            repo = "nixpkgs";
+            rev = "2620ac69c01adfca54c66730eb3dbbe07e6a3024";
+            sha256 = "08cibk8n7cg3az94yhhbl04hrqbxqhgz7g73fbrr9bydpf0k9in1";
+            };
+        in
+        import pinnedPkgs {}
+    ),
+    audioSupport ? true,
+    movieSupport ? true,
+    freetypeSupport ? true,
+    buildDocs ? false,
+    enableUnitTests ? false,
+    # buildAnimView ? false,
+}:
+
+with pkgs.lib;
+
+let
+    lua = {
+        env = pkgs.lua5_3.withPackages(ps: with ps; [ lpeg luafilesystem luasocket ]);
+        packageDir = "${lua.env.outPath}/lib/lua/5.3";
+    };
+
+in pkgs.stdenv.mkDerivation {
+    name = "corsixth";
+    version = "0.64";
+
+    src = ./.;
+
+    nativeBuildInputs = [
+        pkgs.cmake
+    ];
+
+    buildInputs = [
+        lua.env
+        pkgs.SDL2
+    ]
+        ++ optional audioSupport pkgs.SDL2_mixer
+        ++ optionals (audioSupport && pkgs.stdenv.isLinux) [ pkgs.timidity pkgs.freepats ]
+        ++ optional freetypeSupport pkgs.freetype
+        ++ optional movieSupport pkgs.ffmpeg
+        ++ optional buildDocs pkgs.doxygen
+        ++ optional enableUnitTests pkgs.catch2
+        #++ optional buildAnimView pkgs.wxGTK
+    ;
+
+    LUA_DIR = lua.env.outPath;
+    SDL_LIBRARY = "${pkgs.SDL2.outPath}/lib/libSDL2.so";
+    SDL_INCLUDE_DIR = "${pkgs.SDL2.dev.outPath}/include/SDL2";
+    
+    FFMPEG_DIR = optional movieSupport pkgs.ffmpeg.outPath;
+    FREETYPE_DIR = optional freetypeSupport pkgs.freetype.outPath;
+    SDL_MIXER_DIR = optional audioSupport pkgs.SDL2_mixer.outPath;
+    # wxWidgets_ROOT_DIR = pkgs.wxGTK.outPath;
+    
+    cmakeFlags = [ ]
+        ++ optional (!audioSupport) "-DWITH_AUDIO=OFF"
+        ++ optional (!freetypeSupport) "-DWITH_FREETYPE2=OFF"
+        ++ optional (!movieSupport) "-DWITH_MOVIES=OFF"
+        ++ optional enableUnitTests "-DENABLE_UNIT_TESTS=ON"
+        # ++ optional (buildAnimView) "-DBUILD_ANIMVIEW=ON"
+    ;
+
+    preFixup = ''
+        cp -a "${lua.packageDir}"/. $out/share/corsix-th/
+    '';
+
+    shellHook = ''
+        mkdir -p build
+        cp -a "${lua.packageDir}"/. build/
+    '';
+
+    meta = with pkgs.lib; {
+        description = "Open source clone of Theme Hospital";
+        longDescription = "A reimplementation of the 1997 Bullfrog business sim Theme Hospital. As well as faithfully recreating the original, CorsixTH adds support for modern operating systems (Windows, macOS, Linux and BSD), high resolutions and much more.";
+        homepage = "https://github.com/CorsixTH/CorsixTH";
+        license = licenses.mit;
+        platforms = platforms.linux;
+    };
+}
