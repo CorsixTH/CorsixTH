066907e598e0eec9bf4034acdcbb048de3e4f941
Robert Flack <flackr@gmail.com>
2021-06-13 16:28:04 -0400

    Use screen sized zoom buffer for direct_zoom.

--- CorsixTH/Src/th_gfx_sdl.cpp
+++ CorsixTH/Src/th_gfx_sdl.cpp
@@ -389,6 +389,7 @@ bool render_target::set_scale_factor(double fScale, scaled_items eWhatToScale) {
     return false;
   } else if (eWhatToScale == scaled_items::all && direct_zoom) {
     global_scale_factor = fScale;
+    init_zoom_buffer(width, height);
     return true;
   } else if (eWhatToScale == scaled_items::all && supports_target_textures) {
     // Draw everything from now until the next scale to zoom_texture
@@ -397,24 +398,12 @@ bool render_target::set_scale_factor(double fScale, scaled_items eWhatToScale) {
     int virtWidth = static_cast<int>(width / fScale);
     int virtHeight = static_cast<int>(height / fScale);
 
-    zoom_texture =
-        SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ABGR8888,
-                          SDL_TEXTUREACCESS_TARGET, virtWidth, virtHeight);
-
-    SDL_RenderSetLogicalSize(renderer, virtWidth, virtHeight);
-    if (SDL_SetRenderTarget(renderer, zoom_texture) != 0) {
+    if (!init_zoom_buffer(virtWidth, virtHeight)) {
       std::cout << "Warning: Could not render to zoom texture - "
                 << SDL_GetError() << std::endl;
 
-      SDL_RenderSetLogicalSize(renderer, width, height);
-      SDL_DestroyTexture(zoom_texture);
-      zoom_texture = nullptr;
       return false;
     }
-
-    // Clear the new texture to transparent/black.
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_TRANSPARENT);
-    SDL_RenderClear(renderer);
 
     return true;
   } else if (0.999 <= fScale && fScale <= 1.001) {
@@ -595,6 +584,26 @@ bool render_target::take_screenshot(const char* sFile) {
 bool render_target::should_scale_bitmaps(double* pFactor) {
   if (!scale_bitmaps) return false;
   if (pFactor) *pFactor = bitmap_scale_factor;
+  return true;
+}
+
+bool render_target::init_zoom_buffer(int iWidth, int iHeight) {
+  if (!supports_target_textures)
+    return false;
+  zoom_texture =
+      SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ABGR8888,
+                        SDL_TEXTUREACCESS_TARGET, iWidth, iHeight);
+  SDL_RenderSetLogicalSize(renderer, iWidth, iHeight);
+  if (SDL_SetRenderTarget(renderer, zoom_texture) != 0) {
+    SDL_RenderSetLogicalSize(renderer, width, height);
+    SDL_DestroyTexture(zoom_texture);
+    zoom_texture = nullptr;
+    return false;
+  }
+
+  // Clear the new texture to transparent/black.
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_TRANSPARENT);
+  SDL_RenderClear(renderer);
   return true;
 }
 
