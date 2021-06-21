1990777191d4850703d851bf33bcffdfbe4cf8b6
Robert Flack <flackr@gmail.com>
2021-06-21 11:28:21 -0400

    Current year in copyright

--- CorsixTH/Src/th_gfx_common.h
+++ CorsixTH/Src/th_gfx_common.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2012-2013 Luis "Driver" Duarte and Robert "flackr" Flack
+Copyright (C) 2021 Luis "Driver" Duarte and Robert "flackr" Flack
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
