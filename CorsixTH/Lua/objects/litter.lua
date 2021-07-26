6b10967bd1089280ab4ab12aee1b5801d32ca19a
lewri <lewri@users.noreply.github.com>
2021-07-26 19:01:20 +0100

    Update CorsixTH/Lua/objects/litter.lua

--- CorsixTH/Lua/objects/litter.lua
+++ CorsixTH/Lua/objects/litter.lua
@@ -159,7 +159,7 @@ function Litter:afterLoad(old, new)
   if old < 151 then
     self.hospital = self.world:getHospital(self.tile_x, self.tile_y)
   end
-  if old < 159 then
+  if old < 162 then
     -- Afterload 151 could cause nil hospital if the litter was outside it.
     -- For ease, remove affected litter
     if not self.hospital then
