c5b89d7db4e36a59496b723d0c4f4812b0ae07b0
Alberth <alberth289346@gmail.com>
2022-04-24 11:01:22 +0200

    Hospitals store their tile object counts.

--- CorsixTH/Lua/app.lua
+++ CorsixTH/Lua/app.lua
@@ -29,7 +29,7 @@ local runDebugger = corsixth.require("run_debugger")
 -- and add compatibility code in afterLoad functions
 -- Recommended: Also replace/Update the summary comment
 
-local SAVEGAME_VERSION = 166 -- Defines the graphics set used.
+local SAVEGAME_VERSION = 167 -- Let hospitals count their tile objects.
 
 class "App"
 
