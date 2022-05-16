d1c9d36399f8e36e77277f64e561e7595526195e
lewri <lewri@users.noreply.github.com>
2022-05-16 17:52:13 +0100

    Seek reception typo (#2162)
    * Seek reception typo
    Correcting an error not caught in #2125

--- CorsixTH/Lua/humanoid_actions/seek_reception.lua
+++ CorsixTH/Lua/humanoid_actions/seek_reception.lua
@@ -75,12 +75,12 @@ local function action_seek_reception_start(action, humanoid)
     local dist = humanoid.world:getPathDistance(humanoid.tile_x, humanoid.tile_y, x, y)
     -- TODO: Make 'else' method work for all humanoids
     if class.is(humanoid, Vip) or class.is(humanoid, Inspector) then
-      humanoid:updateDynamicInfo(_S.dynamic_info.patient.actions_on_my_way_to
-      :format(best_desk.object_type.name))
+      humanoid:updateDynamicInfo(_S.dynamic_info.patient.actions.on_my_way_to
+          :format(best_desk.object_type.name))
 
     else
       humanoid:setDynamicInfoText(_S.dynamic_info.patient.actions.on_my_way_to
-      :format(best_desk.object_type.name))
+          :format(best_desk.object_type.name))
     end
     humanoid.waiting = nil
 
