788b9819c8dd17cb16adc9308be8e097bbf7b1ec
Stephen E. Baker <baker.stephen.e@gmail.com>
2022-10-18 22:04:07 -0400

    Bump vcpkg commit tag
    The biggest change is the bump from SDL2_mixer 2.0.4 to 2.6.1
    flac, ogg vorbis, and mp3 support are now all built in without
    extra libraries.
    
    There is now a fluidsynth option for sdl2_mixer in vcpkg, but
    this commit does not enable it.

--- CMake/CopyVcpkgSdlMixerBackends.cmake
+++ CMake/CopyVcpkgSdlMixerBackends.cmake
@@ -1,12 +1,5 @@
 add_custom_command(TARGET CorsixTH POST_BUILD
   COMMAND ${CMAKE_COMMAND} -E copy
-  "${VCPKG_INSTALLED_PATH}/$<$<CONFIG:Debug>:debug/>bin/FLAC.dll"
-  "${VCPKG_INSTALLED_PATH}/$<$<CONFIG:Debug>:debug/>bin/FLAC++.dll"
-  "${VCPKG_INSTALLED_PATH}/$<$<CONFIG:Debug>:debug/>bin/mpg123.dll"
   "${VCPKG_INSTALLED_PATH}/$<$<CONFIG:Debug>:debug/>bin/modplug.dll"
-  "${VCPKG_INSTALLED_PATH}/$<$<CONFIG:Debug>:debug/>bin/ogg.dll"
-  "${VCPKG_INSTALLED_PATH}/$<$<CONFIG:Debug>:debug/>bin/vorbis.dll"
-  "${VCPKG_INSTALLED_PATH}/$<$<CONFIG:Debug>:debug/>bin/vorbisenc.dll"
-  "${VCPKG_INSTALLED_PATH}/$<$<CONFIG:Debug>:debug/>bin/vorbisfile.dll"
   $<TARGET_FILE_DIR:CorsixTH>
 )
