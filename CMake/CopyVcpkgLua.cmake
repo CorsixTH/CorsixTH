f2204b4e17bb5a35810fb4fe35d3821b6adfa2cf
Stephen E. Baker <baker.stephen.e@gmail.com>
2024-04-29 22:45:38 -0400

    Replace CopyVcpkgLua

--- CMake/CopyVcpkgLua.cmake
+++
@@ -1,6 +0,0 @@
-# Add an extra step to copy LUA files from vcpkg
-add_custom_command(TARGET CorsixTH POST_BUILD
-  COMMAND ${CMAKE_COMMAND} -E copy
-  "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/share/lua/re.lua"
-  $<TARGET_FILE_DIR:CorsixTH>
-)
