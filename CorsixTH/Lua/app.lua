17efff1e9a9a82195e4ebe9f0ea9033d47fda66f
Toby Lane <mrtobylane@gmail.com>
2024-06-28 13:03:39 +0100

    Close progress report

--- CorsixTH/Lua/app.lua
+++ CorsixTH/Lua/app.lua
@@ -29,7 +29,7 @@ local runDebugger = corsixth.require("run_debugger")
 -- and add compatibility code in afterLoad functions
 -- Recommended: Also replace/Update the summary comment
 
-local SAVEGAME_VERSION = 190 -- Improve falling actions for debug purposes
+local SAVEGAME_VERSION = 191 -- Add support for new win and lose criteria
 
 class "App"
 
