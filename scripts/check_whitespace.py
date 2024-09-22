730e83f5d793ffaf123b6d40e7eddf4e2f1a9c6c
Alberth <alberth289346@gmail.com>
2024-09-22 11:31:39 +0200

    Generalize the RE pattern to find both TAB and trailing white space.

--- scripts/check_whitespace.py
+++ scripts/check_whitespace.py
@@ -1,34 +1,45 @@
 #!/usr/bin/env python3
 
 """
-  Usage: check_trailing_whitespaces.py [root]
-  This script will check the presence of trailing whitespaces in any file
-  below |root|. It will return 0 if none is found. Otherwise, it will print the
-  path of the violating file and return an error code.
-  If root is not specified, it will use the current directory.
+  Usage: check_whitespace.py [root-dir]
+
+  This script will check for incorrect whitespace (usage of TAB and trailing
+  whitespace) in Lua, Python, and C/C++ files below |root-dir|.
+    It will return 0 (ie success) if only correct whitespace is found.
+  Otherwise, it will print the path of the violating file(s) and return an
+  error code.
+
+  If root-dir is not specified, it will use the current directory.
 """
 
 import os
 import re
 import sys
 
-TRAILING_SEQUENCE = re.compile(r'[ \t][\r\n]')
+# Bad whitespace is
+# - a TAB (anywhere),
+# - a SPACE before CR,
+# - a SPACE before NL, or
+# - a SPACE as last character.
+BAD_WHITESPACE = re.compile(r'\t| \r| \n| $')
 
 
-def has_trailing_whitespace(path):
-    """ Returns whether |path| has trailing whitespace. """
+def has_bad_whitespace(path):
+    """
+    Returns whether the file at |path| has bad whitespace.
+    """
     if os.path.isfile(path):
         with open(path, 'r') as handle:
             for line in handle:
-                m = TRAILING_SEQUENCE.search(line)
+                m = BAD_WHITESPACE.search(line)
                 if m:
                     return True
 
     return False
 
-
+# Main program.
 if len(sys.argv) > 2:
-    sys.exit('Usage: {} [root]'.format(sys.argv[0]))
+    sys.exit(f'Usage: {sys.argv[0]} [root-dir]')
 
 top = os.getcwd()
 if len(sys.argv) == 2:
@@ -38,15 +49,15 @@ count = 0
 offending_files = []
 for root, dirs, files in os.walk(top):
     for f in files:
-        if f.endswith(('.py', '.lua', '.h', '.cpp', '.cc', '.c')):
+        if f.lower().endswith(('.py', '.lua', '.h', '.cpp', '.cc', '.c')):
             count += 1
             path = os.path.join(root, f)
-            if has_trailing_whitespace(path):
+            if has_bad_whitespace(path):
                 offending_files.append(path)
 
 print('Checked {} files'.format(count))
 if offending_files:
-    print('Found files with trailing whitespace:')
+    print('Found files with bad whitespace:')
     for path in offending_files:
         print(path)
     sys.exit(1)
