b35a2d430570407c0a157d16645eaab524bc3d89
Matroftt <91145555+Matroftt@users.noreply.github.com>
2024-11-28 22:40:09 +0300

    Improved code

--- CorsixTH/Lua/dialogs/staff_dialog.lua
+++ CorsixTH/Lua/dialogs/staff_dialog.lua
@@ -72,6 +72,7 @@ function UIStaff:UIStaff(ui, staff)
   else
     self.height = 304
   end
+  self.name_box = {x_left = 19, x_right = 169, y_top = 20, y_btm = 41}
   self:setDefaultPosition(-20, 30)
   self.panel_sprites = app.gfx:loadSpriteTable("QData", "Req01V", true)
   self.white_font = app.gfx:loadFont("QData", "Font01V")
@@ -259,14 +260,20 @@ function UIStaff:onMouseUp(button, x, y)
     self.do_scroll = false
   end
   local repaint = Window.onMouseUp(self, button, x, y)
-  -- Test for hit within the view circle
+  -- Test for hit within the view circle and name box
+  local hit_namebox = x > self.name_box.x_left and x < self.name_box.x_right and y > self.name_box.y_top and y < self.name_box.y_btm
   if button == "right" and is_in_view_circle(x, y, self.staff.profile.humanoid_class == "Handyman")
-     or x > 18 and x < 170 and y > 19 and y < 42 then -- Coordinates of name label
+     or button == "right" and hit_namebox then
     -- Right click goes to the next staff member of the same category (NB: Surgeon in same Category as Doctor)
     local staff_index = nil
     for i, staff in ipairs(ui.hospital.staff) do
       if staff_index and staff.profile.humanoid_class == self.staff.profile.humanoid_class then
         ui:addWindow(UIStaff(ui, staff))
+		    if hit_namebox then
+          local sx, sy = ui.app.map:WorldToScreen(staff.tile_x, staff.tile_y)
+          local dx, dy = staff.th:getPosition()
+          ui:scrollMapTo(sx + dx, sy + dy)
+		    end
         return false
       end
       if staff == self.staff then
@@ -278,6 +285,11 @@ function UIStaff:onMouseUp(button, x, y)
       local staff = ui.hospital.staff[i]
       if staff.profile.humanoid_class == self.staff.profile.humanoid_class then
         ui:addWindow(UIStaff(ui, staff))
+		    if hit_namebox then
+          local sx, sy = ui.app.map:WorldToScreen(staff.tile_x, staff.tile_y)
+          local dx, dy = staff.th:getPosition()
+          ui:scrollMapTo(sx + dx, sy + dy)
+		    end
         return false
       end
     end
@@ -391,4 +403,3 @@ function UIStaff:afterLoad(old, new)
   end
   Window.afterLoad(self, old, new)
 end
-
