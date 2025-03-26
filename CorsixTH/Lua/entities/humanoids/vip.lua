44c5dbc690666ce3ffca3b0ed1a51affb93bc803
Artem Argus Gusakov <arg.amx@gmail.com>
2025-03-26 15:19:00 +0300

    attempt to fix LuaTest

--- CorsixTH/Lua/entities/humanoids/vip.lua
+++ CorsixTH/Lua/entities/humanoids/vip.lua
@@ -207,7 +207,7 @@ function Vip:evaluateRoom()
       room_bin = 1
     end
 
-    if object ~= nil and object:isMachine() then
+    if object:isMachine() then
       self.room_eval = object:isBreaking() and self.room_eval - 1 or self.room_eval + 1
     end
   end
