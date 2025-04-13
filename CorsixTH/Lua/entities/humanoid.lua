a849146bd319384952fea89356b045b72862ce8c
Alberth <alberth289346@gmail.com>
2025-04-13 14:02:09 +0200

    Fix too general name for walk animations.

--- CorsixTH/Lua/entities/humanoid.lua
+++ CorsixTH/Lua/entities/humanoid.lua
@@ -39,7 +39,7 @@ local check_watch_animations = permanent"humanoid_check_watch_animations"({})
 
 local mood_icons = permanent"humanoid_mood_icons"({})
 
-local function anims(name, walkN, walkE, idleN, idleE, doorL, doorE, knockN, knockE, swingL, swingE)
+local function walk_anims(name, walkN, walkE, idleN, idleE, doorL, doorE, knockN, knockE, swingL, swingE)
   walk_animations[name] = {
     walk_east = walkE,
     walk_north = walkN,
@@ -133,37 +133,37 @@ local function assignStaffMarkers(anims, name, ...)
 end
 
 
---   | Walk animations           |
---   | Name                      |WalkN|WalkE|IdleN|IdleE|DoorL|DoorE|KnockN|KnockE|SwingL|SwingE| Notes
------+---------------------------+-----+-----+-----+-----+-----+-----+------+------+-------+---------+
-anims("Standard Male Patient",       16,   18,   24,   26,  182,  184,   286,   288,  2040,  2042) -- 0-16, ABC
-anims("Gowned Male Patient",        406,  408,  414,  416)                           -- 0-10
-anims("Stripped Male Patient",      818,  820,  826,  828)                           -- 0-16
-anims("Stripped Male Patient 2",    818,  820,  826,  828)                           -- 0-16
-anims("Stripped Male Patient 3",    818,  820,  826,  828)
-anims("Alternate Male Patient",    2704, 2706, 2712, 2714, 2748, 2750,  2764,  2766) -- 0-10, ABC
-anims("Slack Male Patient",        1484, 1486, 1492, 1494, 1524, 1526,  2764,  1494) -- 0-14, ABC
-anims("Slack Female Patient",         0,    2,    8,   10,  258,  260,   294,   296,  2864,  2866) -- 0-16, ABC
-anims("Transparent Male Patient",  1064, 1066, 1072, 1074, 1104, 1106,  1120,  1074) -- 0-16, ABC
-anims("Standard Female Patient",      0,    2,    8,   10,  258,  260,   294,   296,  2864,  2866) -- 0-16, ABC
-anims("Gowned Female Patient",     2876, 2878, 2884, 2886)                           -- 0-8
-anims("Stripped Female Patient",    834,  836,  842,  844)                           -- 0-16
-anims("Stripped Female Patient 2",  834,  836,  842,  844)                           -- 0-16
-anims("Stripped Female Patient 3",  834,  836,  842,  844)
-anims("Transparent Female Patient",3012, 3014, 3020, 3022, 3052, 3054,  3068,  3070) -- 0-8, ABC
-anims("Chewbacca Patient",          858,  860,  866,  868, 3526, 3528,  4150,  4152)
-anims("Elvis Patient",              978,  980,  986,  988, 3634, 3636,  4868,  4870)
-anims("Invisible Patient",         1642, 1644, 1840, 1842, 1796, 1798,  4192,  4194)
-anims("Alien Male Patient",        3598, 3600, 3606, 3608,  182,  184,   286,   288, 3626,  3628) -- remember, no "normal"-doors animation
-anims("Alien Female Patient",      3598, 3600, 3606, 3608,  258,  260,   294,   296, 3626,  3628) -- identical to male; however death animations differ
-anims("Doctor",                      32,   34,   40,   42,  670,  672,   nil,   nil, 4750,  4752)
-anims("Surgeon",                   2288, 2290, 2296, 2298)
-anims("Nurse",                     1206, 1208, 1650, 1652, 3264, 3266,   nil,   nil, 3272,  3274)
-anims("Handyman",                  1858, 1860, 1866, 1868, 3286, 3288,   nil,   nil, 3518,  3520)
-anims("Receptionist",              3668, 3670, 3676, 3678) -- Could do with door animations
-anims("VIP",                        266,  268,  274,  276)
-anims("Inspector",                  266,  268,  274,  276)
-anims("Grim Reaper",                994,  996, 1002, 1004)
+--   | Walk animations                 |
+--   | Name                            |WalkN|WalkE|IdleN|IdleE|DoorL|DoorE|KnockN|KnockE|SwingL|SwingE| Notes
+-----+---------------------------------------+-----+-----+-----+-----+-----+-----+------+------+-------+---------+
+walk_anims("Standard Male Patient",       16,   18,   24,   26,  182,  184,   286,   288,  2040,  2042) -- 0-16, ABC
+walk_anims("Gowned Male Patient",        406,  408,  414,  416)                           -- 0-10
+walk_anims("Stripped Male Patient",      818,  820,  826,  828)                           -- 0-16
+walk_anims("Stripped Male Patient 2",    818,  820,  826,  828)                           -- 0-16
+walk_anims("Stripped Male Patient 3",    818,  820,  826,  828)
+walk_anims("Alternate Male Patient",    2704, 2706, 2712, 2714, 2748, 2750,  2764,  2766) -- 0-10, ABC
+walk_anims("Slack Male Patient",        1484, 1486, 1492, 1494, 1524, 1526,  2764,  1494) -- 0-14, ABC
+walk_anims("Slack Female Patient",         0,    2,    8,   10,  258,  260,   294,   296,  2864,  2866) -- 0-16, ABC
+walk_anims("Transparent Male Patient",  1064, 1066, 1072, 1074, 1104, 1106,  1120,  1074) -- 0-16, ABC
+walk_anims("Standard Female Patient",      0,    2,    8,   10,  258,  260,   294,   296,  2864,  2866) -- 0-16, ABC
+walk_anims("Gowned Female Patient",     2876, 2878, 2884, 2886)                           -- 0-8
+walk_anims("Stripped Female Patient",    834,  836,  842,  844)                           -- 0-16
+walk_anims("Stripped Female Patient 2",  834,  836,  842,  844)                           -- 0-16
+walk_anims("Stripped Female Patient 3",  834,  836,  842,  844)
+walk_anims("Transparent Female Patient",3012, 3014, 3020, 3022, 3052, 3054,  3068,  3070) -- 0-8, ABC
+walk_anims("Chewbacca Patient",          858,  860,  866,  868, 3526, 3528,  4150,  4152)
+walk_anims("Elvis Patient",              978,  980,  986,  988, 3634, 3636,  4868,  4870)
+walk_anims("Invisible Patient",         1642, 1644, 1840, 1842, 1796, 1798,  4192,  4194)
+walk_anims("Alien Male Patient",        3598, 3600, 3606, 3608,  182,  184,   286,   288, 3626,  3628) -- remember, no "normal"-doors animation
+walk_anims("Alien Female Patient",      3598, 3600, 3606, 3608,  258,  260,   294,   296, 3626,  3628) -- identical to male; however death animations differ
+walk_anims("Doctor",                      32,   34,   40,   42,  670,  672,   nil,   nil, 4750,  4752)
+walk_anims("Surgeon",                   2288, 2290, 2296, 2298)
+walk_anims("Nurse",                     1206, 1208, 1650, 1652, 3264, 3266,   nil,   nil, 3272,  3274)
+walk_anims("Handyman",                  1858, 1860, 1866, 1868, 3286, 3288,   nil,   nil, 3518,  3520)
+walk_anims("Receptionist",              3668, 3670, 3676, 3678) -- Could do with door animations
+walk_anims("VIP",                        266,  268,  274,  276)
+walk_anims("Inspector",                  266,  268,  274,  276)
+walk_anims("Grim Reaper",                994,  996, 1002, 1004)
 
 local doorE_markers = { -- Anim 3288 in particular
   {-19, -15, "px"}, {-26, -17, "px"}, {-26, -17, "px"}, {-26, -17, "px"},
