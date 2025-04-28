CorsixTH/CorsixTH

commit 5ff228ac21caeae6aba876728b9b36da9f0a1f5a

Refactor drawable draw / hit_test / is_multiple_frame_animation methods (#2808)
* Refactor drawable draw/hit_test/is_multiple_frame_animation

- Replaced the 'drawable' base class function pointers by abstract virtual methods.
- Implemented them in 'animation' and 'sprite_render_list'.
- Added an enumeration to 'animation' to track the kind of animation that is stored.

- Deleted all the functions of the removed function pointers.

* Fix converting lua userdata to animation

reinterpret_cast and static_cast from a void pointer to any type
other than the original is not allowed in c++, there is no
guarantee that the memory is the same, and as soon as virtual
functions are involved it is not.

This fixes that defect as far as it affected this patch by taking
advantage of our metadata to extract the right type of class.

---------

Co-authored-by: Stephen E. Baker <baker.stephen.e@gmail.com>

CorsixTH/Src/th_gfx.cpp
@@ -1249,55 +1249,6 @@ bool animation::hit_test_morph(int iDestX, int iDestY, int iTestX, int iTestY) {
          morph_target->hit_test(iDestX, iDestY, iTestX, iTestY);
 }
 
-namespace {
-bool THAnimation_hit_test_child(drawable* pSelf, int iDestX, int iDestY,
-                                int iTestX, int iTestY) {
-  return reinterpret_cast<animation*>(pSelf)->hit_test_child(iDestX, iDestY,
-                                                             iTestX, iTestY);
-}
-
-void THAnimation_draw_child(drawable* pSelf, render_target* pCanvas, int iDestX,
-                            int iDestY) {
-  reinterpret_cast<animation*>(pSelf)->draw_child(pCanvas, iDestX, iDestY);
-}
-
-bool THAnimation_hit_test_morph(drawable* pSelf, int iDestX, int iDestY,
-                                int iTestX, int iTestY) {
-  return reinterpret_cast<animation*>(pSelf)->hit_test_morph(iDestX, iDestY,
-                                                             iTestX, iTestY);
-}
-
-void THAnimation_draw_morph(drawable* pSelf, render_target* pCanvas, int iDestX,
-                            int iDestY) {
-  reinterpret_cast<animation*>(pSelf)->draw_morph(pCanvas, iDestX, iDestY);
-}
-
-bool THAnimation_hit_test(drawable* pSelf, int iDestX, int iDestY, int iTestX,
-                          int iTestY) {
-  return reinterpret_cast<animation*>(pSelf)->hit_test(iDestX, iDestY, iTestX,
-                                                       iTestY);
-}
-
-void THAnimation_draw(drawable* pSelf, render_target* pCanvas, int iDestX,
-                      int iDestY) {
-  reinterpret_cast<animation*>(pSelf)->draw(pCanvas, iDestX, iDestY);
-}
-
-bool THAnimation_is_multiple_frame_animation(drawable* pSelf) {
-  animation* pAnimation = reinterpret_cast<animation*>(pSelf);
-  if (pAnimation) {
-    size_t firstFrame = pAnimation->get_animation_manager()->get_first_frame(
-        pAnimation->get_animation());
-    size_t nextFrame =
-        pAnimation->get_animation_manager()->get_next_frame(firstFrame);
-    return nextFrame != firstFrame;
-  } else {
-    return false;
-  }
-}
-
-}  // namespace
-
 animation_base::animation_base() : drawable() {
   x_relative_to_tile = 0;
   y_relative_to_tile = 0;
@@ -1315,10 +1266,8 @@ animation::animation()
       frame_index(0),
       speed({0, 0}),
       sound_to_play(0),
-      crop_column(0) {
-  draw_fn = THAnimation_draw;
-  hit_test_fn = THAnimation_hit_test;
-  is_multiple_frame_animation_fn = THAnimation_is_multiple_frame_animation;
+      crop_column(0),
+      anim_kind(animation_kind::normal) {
   patient_effect = animation_effect::none;
   patient_effect_offset = rand();
 }
@@ -1336,13 +1285,11 @@ void animation::persist(lua_persist_writer* pWriter) const {
   // Write the drawable fields
   pWriter->write_uint(flags);
 
-  if (draw_fn == THAnimation_draw && hit_test_fn == THAnimation_hit_test) {
+  if (anim_kind == animation_kind::normal) {
     pWriter->write_uint(1);
-  } else if (draw_fn == THAnimation_draw_child &&
-             hit_test_fn == THAnimation_hit_test_child) {
+  } else if (anim_kind == animation_kind::child) {
     pWriter->write_uint(2);
-  } else if (draw_fn == THAnimation_draw_morph &&
-             hit_test_fn == THAnimation_hit_test_morph) {
+  } else if (anim_kind == animation_kind::morph) {
     // NB: Prior version of code used the number 3 here, and forgot
     // to persist the morph target.
     pWriter->write_uint(4);
@@ -1371,7 +1318,7 @@ void animation::persist(lua_persist_writer* pWriter) const {
   }
 
   // Write the unioned fields
-  if (draw_fn != THAnimation_draw_child) {
+  if (anim_kind != animation_kind::child) {
     pWriter->write_int(speed.dx);
     pWriter->write_int(speed.dy);
   } else {
@@ -1397,7 +1344,8 @@ void animation::depersist(lua_persist_reader* pReader) {
   do {
     // Read the chain
     if (!pReader->read_stack_object()) break;
-    next = reinterpret_cast<link_list*>(lua_touserdata(L, -1));
+
+    next = luaT_testuserdata<animation_base>(L, -1, false);
     if (next) next->prev = this;
     lua_pop(L, 1);
 
@@ -1411,16 +1359,13 @@ void animation::depersist(lua_persist_reader* pReader) {
         // missing, so settle for a graphical bug rather than a segfault
         // by reverting to the normal function set.
       case 1:
-        draw_fn = THAnimation_draw;
-        hit_test_fn = THAnimation_hit_test;
+        set_animation_kind(animation_kind::normal);
         break;
       case 2:
-        draw_fn = THAnimation_draw_child;
-        hit_test_fn = THAnimation_hit_test_child;
+        set_animation_kind(animation_kind::child);
         break;
       case 4:
-        draw_fn = THAnimation_draw_morph;
-        hit_test_fn = THAnimation_hit_test_morph;
+        set_animation_kind(animation_kind::morph);
         pReader->read_stack_object();
         morph_target = reinterpret_cast<animation*>(lua_touserdata(L, -1));
         lua_pop(L, 1);
@@ -1450,7 +1395,7 @@ void animation::depersist(lua_persist_reader* pReader) {
     }
 
     // Read the unioned fields
-    if (draw_fn != THAnimation_draw_child) {
+    if (anim_kind != animation_kind::child) {
       if (!pReader->read_int(speed.dx)) break;
       if (!pReader->read_int(speed.dy)) break;
     } else {
@@ -1494,6 +1439,10 @@ void animation::set_patient_effect(animation_effect patient_effect) {
   this->patient_effect = patient_effect;
 }
 
+void animation::set_animation_kind(animation_kind anim_kind) {
+  this->anim_kind = anim_kind;
+}
+
 namespace {
 // Map of frame numbers to sounds to play.
 const std::map<size_t, int> frame_sound_replacements{
@@ -1516,7 +1465,7 @@ const std::map<size_t, int> frame_sound_replacements{
 
 void animation::tick() {
   frame_index = manager->get_next_frame(frame_index);
-  if (draw_fn != THAnimation_draw_child) {
+  if (anim_kind != animation_kind::child) {
     x_relative_to_tile += speed.dx;
     y_relative_to_tile += speed.dy;
   }
@@ -1569,12 +1518,10 @@ void animation_base::attach_to_tile(map_tile* pMapNode, int layer) {
 void animation::set_parent(animation* pParent) {
   remove_from_tile();
   if (pParent == nullptr) {
-    draw_fn = THAnimation_draw;
-    hit_test_fn = THAnimation_hit_test;
+    set_animation_kind(animation_kind::normal);
     speed = {0, 0};
   } else {
-    draw_fn = THAnimation_draw_child;
-    hit_test_fn = THAnimation_hit_test_child;
+    set_animation_kind(animation_kind::child);
     parent = pParent;
     next = parent->next;
     if (next) next->prev = this;
@@ -1589,8 +1536,7 @@ void animation::set_animation(animation_manager* pManager, size_t iAnimation) {
   frame_index = pManager->get_first_frame(iAnimation);
   if (morph_target) {
     morph_target = nullptr;
-    draw_fn = THAnimation_draw;
-    hit_test_fn = THAnimation_hit_test;
+    set_animation_kind(animation_kind::normal);
   }
 }
 
@@ -1654,8 +1600,7 @@ int GetAnimationDurationAndExtent(animation_manager* pManager, size_t iFrame,
 
 void animation::set_morph_target(animation* pMorphTarget, int iDurationFactor) {
   morph_target = pMorphTarget;
-  draw_fn = THAnimation_draw_morph;
-  hit_test_fn = THAnimation_hit_test_morph;
+  set_animation_kind(animation_kind::morph);
 
   /* Morphing is the process by which two animations are combined to give a
   single animation of one animation turning into another. At the moment,
@@ -1712,31 +1657,7 @@ void animation_base::set_layer(int iLayer, int iId) {
   }
 }
 
-namespace {
-
-bool THSpriteRenderList_hit_test(drawable* pSelf, int iDestX, int iDestY,
-                                 int iTestX, int iTestY) {
-  return reinterpret_cast<sprite_render_list*>(pSelf)->hit_test(iDestX, iDestY,
-                                                                iTestX, iTestY);
-}
-
-void THSpriteRenderList_draw(drawable* pSelf, render_target* pCanvas,
-                             int iDestX, int iDestY) {
-  reinterpret_cast<sprite_render_list*>(pSelf)->draw(pCanvas, iDestX, iDestY);
-}
-
-bool THSpriteRenderList_is_multiple_frame_animation(drawable* pSelf) {
-  return false;
-}
-
-}  // namespace
-
-sprite_render_list::sprite_render_list() : animation_base() {
-  draw_fn = THSpriteRenderList_draw;
-  hit_test_fn = THSpriteRenderList_hit_test;
-  is_multiple_frame_animation_fn =
-      THSpriteRenderList_is_multiple_frame_animation;
-}
+sprite_render_list::sprite_render_list() : animation_base() {}
 
 sprite_render_list::~sprite_render_list() { delete[] sprites; }
 
@@ -1907,7 +1828,7 @@ void sprite_render_list::depersist(lua_persist_reader* pReader) {
     return;
   }
 
-  next = reinterpret_cast<link_list*>(lua_touserdata(L, -1));
+  next = luaT_testuserdata<animation_base>(L, -1, false);
   if (next) {
     next->prev = this;
   }

CorsixTH/Src/th_gfx.h
@@ -30,6 +30,7 @@ SOFTWARE.
 #include "th.h"
 #include "th_gfx_common.h"
 #include "th_gfx_sdl.h"
+#include "th_lua.h"
 
 class lua_persist_reader;
 class lua_persist_writer;
@@ -109,27 +110,25 @@ struct drawable : public link_list {
   /*!
       Can also "draw" the object to the speakers, i.e. play sounds.
   */
-  void (*draw_fn)(drawable* pSelf, render_target* pCanvas, int iDestX,
-                  int iDestY);
+  virtual void draw_fn(render_target* pCanvas, int iDestX, int iDestY) = 0;
 
   //! Perform a hit test against the object
   /*!
       Should return true if when the object is drawn at (iDestX, iDestY) on a
      canvas, the point (iTestX, iTestY) is within / on the object.
   */
-  bool (*hit_test_fn)(drawable* pSelf, int iDestX, int iDestY, int iTestX,
-                      int iTestY);
-
-  //! Drawing flags (zero or more list flags from #draw_flags).
-  uint32_t flags;
+  virtual bool hit_test_fn(int iDestX, int iDestY, int iTestX, int iTestY) = 0;
 
   /** Returns true if instance is a multiple frame animation.
       Should be overloaded in derived class.
   */
-  bool (*is_multiple_frame_animation_fn)(drawable* pSelf);
+  virtual bool is_multiple_frame_animation_fn() = 0;
 
   int get_drawing_layer() { return drawing_layer; }
   void set_drawing_layer(int layer) { drawing_layer = layer; }
+
+  //! Drawing flags (zero or more list flags from #draw_flags).
+  uint32_t flags;
 
  private:
   int drawing_layer;
@@ -541,7 +540,6 @@ class animation_base : public drawable {
   void set_layer(int iLayer, int iId);
   void set_layers_from(const animation_base* pSrc) { layers = pSrc->layers; }
 
-  // bool isMultipleFrameAnimation() { return false;}
  protected:
   //! X position on tile (not tile x-index)
   int x_relative_to_tile;
@@ -558,6 +556,9 @@ struct xy_diff {
   int dy;
 };
 
+//! The kind of animation.
+enum class animation_kind { normal, child, morph };
+
 class animation : public animation_base {
  public:
   animation();
@@ -571,6 +572,39 @@ class animation : public animation_base {
   bool hit_test_morph(int iDestX, int iDestY, int iTestX, int iTestY);
   void draw_child(render_target* pCanvas, int iDestX, int iDestY);
   bool hit_test_child(int iDestX, int iDestY, int iTestX, int iTestY);
+
+  void draw_fn(render_target* pCanvas, int iDestX, int iDestY) override {
+    switch (anim_kind) {
+      case animation_kind::normal:
+        draw(pCanvas, iDestX, iDestY);
+        return;
+      case animation_kind::child:
+        draw_child(pCanvas, iDestX, iDestY);
+        return;
+      case animation_kind::morph:
+        draw_morph(pCanvas, iDestX, iDestY);
+        return;
+    }
+  }
+
+  bool hit_test_fn(int iDestX, int iDestY, int iTestX, int iTestY) override {
+    switch (anim_kind) {
+      case animation_kind::normal:
+        return hit_test(iDestX, iDestY, iTestX, iTestY);
+      case animation_kind::child:
+        return hit_test_child(iDestX, iDestY, iTestX, iTestY);
+      case animation_kind::morph:
+        return hit_test_morph(iDestX, iDestY, iTestX, iTestY);
+    }
+    return false;
+  }
+
+  bool is_multiple_frame_animation_fn() override {
+    size_t firstFrame =
+        get_animation_manager()->get_first_frame(get_animation());
+    size_t nextFrame = get_animation_manager()->get_next_frame(firstFrame);
+    return nextFrame != firstFrame;
+  }
 
   link_list* get_previous() { return prev; }
   size_t get_animation() const { return animation_index; }
@@ -590,6 +624,8 @@ class animation : public animation_base {
   void depersist(lua_persist_reader* pReader);
 
   void set_patient_effect(animation_effect patient_effect);
+  void set_animation_kind(animation_kind anim_kind);
+  animation_kind get_animation_kind() { return anim_kind; }
 
   animation_manager* get_animation_manager() { return manager; }
 
@@ -607,6 +643,7 @@ class animation : public animation_base {
 
   size_t sound_to_play;
   int crop_column;
+  animation_kind anim_kind;
   animation_effect patient_effect;
   //! Number of game_ticks to offset animation by so they aren't all
   //! running in sync.
@@ -632,6 +669,16 @@ class sprite_render_list : public animation_base {
   void persist(lua_persist_writer* pWriter) const;
   void depersist(lua_persist_reader* pReader);
 
+  void draw_fn(render_target* pCanvas, int iDestX, int iDestY) override {
+    draw(pCanvas, iDestX, iDestY);
+  }
+
+  bool hit_test_fn(int iDestX, int iDestY, int iTestX, int iTestY) override {
+    return hit_test(iDestX, iDestY, iTestX, iTestY);
+  }
+
+  bool is_multiple_frame_animation_fn() override { return false; }
+
  private:
   struct sprite {
     size_t index;
@@ -654,5 +701,17 @@ class sprite_render_list : public animation_base {
   //! rendering quality when scaling.
   bool use_intermediate_buffer = false;
 };
+
+// Specialization of the 'luaT_testuserdata' template for the 'animation_base'
+// class.
+template <>
+inline animation_base* luaT_testuserdata<animation_base>(lua_State* L, int idx,
+                                                         bool required) {
+  animation_base* p = luaT_testuserdata<animation>(L, idx, false);
+  if (p == nullptr) {
+    p = luaT_testuserdata<sprite_render_list>(L, idx, required);
+  }
+  return p;
+}
 
 #endif  // CORSIX_TH_TH_GFX_H_

CorsixTH/Src/th_map.cpp
@@ -1042,7 +1042,7 @@ void level_map::draw(render_target* pCanvas, int iScreenX, int iScreenY,
       }
       drawable* pItem = static_cast<drawable*>(itrNode->oEarlyEntities.next);
       while (pItem) {
-        pItem->draw_fn(pItem, pCanvas, itrNode.x(), itrNode.y());
+        pItem->draw_fn(pCanvas, itrNode.x(), itrNode.y());
         pItem = static_cast<drawable*>(pItem->next);
       }
     }
@@ -1091,8 +1091,8 @@ void level_map::draw(render_target* pCanvas, int iScreenX, int iScreenY,
 
       drawable* pItem = static_cast<drawable*>(itrNode->entities.next);
       while (pItem) {
-        pItem->draw_fn(pItem, pCanvas, itrNode.x(), itrNode.y());
-        if (pItem->is_multiple_frame_animation_fn(pItem)) {
+        pItem->draw_fn(pCanvas, itrNode.x(), itrNode.y());
+        if (pItem->is_multiple_frame_animation_fn()) {
           bRedrawAnimations = true;
         }
         if (pItem->get_drawing_layer() == 1) {
@@ -1115,7 +1115,7 @@ void level_map::draw(render_target* pCanvas, int iScreenX, int iScreenY,
             formerIterator.get_previous_tile()->entities.next);
         while (pItem) {
           if (pItem->get_drawing_layer() == 9) {
-            pItem->draw_fn(pItem, pCanvas, formerIterator.x() - 64,
+            pItem->draw_fn(pCanvas, formerIterator.x() - 64,
                            formerIterator.y());
             bTileNeedsRedraw = true;
           }
@@ -1129,8 +1129,7 @@ void level_map::draw(render_target* pCanvas, int iScreenX, int iScreenY,
                     : nullptr;
         while (pItem) {
           if (pItem->get_drawing_layer() == 8) {
-            pItem->draw_fn(pItem, pCanvas, formerIterator.x(),
-                           formerIterator.y());
+            pItem->draw_fn(pCanvas, formerIterator.x(), formerIterator.y());
           }
           pItem = static_cast<drawable*>(pItem->next);
         }
@@ -1163,13 +1162,13 @@ void level_map::draw(render_target* pCanvas, int iScreenX, int iScreenY,
           pItem = static_cast<drawable*>(
               itrNode.get_previous_tile()->oEarlyEntities.next);
           for (; pItem; pItem = static_cast<drawable*>(pItem->next)) {
-            pItem->draw_fn(pItem, pCanvas, itrNode.x() - 64, itrNode.y());
+            pItem->draw_fn(pCanvas, itrNode.x() - 64, itrNode.y());
           }
 
           pItem = static_cast<drawable*>(
               itrNode.get_previous_tile()->entities.next);
           for (; pItem; pItem = static_cast<drawable*>(pItem->next)) {
-            pItem->draw_fn(pItem, pCanvas, itrNode.x() - 64, itrNode.y());
+            pItem->draw_fn(pCanvas, itrNode.x() - 64, itrNode.y());
           }
         }
       }
@@ -1245,7 +1244,7 @@ drawable* level_map::hit_test_drawables(link_list* pListStart, int iXs, int iYs,
   drawable* pList = static_cast<drawable*>(pListEnd);
 
   while (true) {
-    if (pList->hit_test_fn(pList, iXs, iYs, iTestX, iTestY)) return pList;
+    if (pList->hit_test_fn(iXs, iYs, iTestX, iTestY)) return pList;
 
     if (pList == pListStart) {
       return nullptr;
@@ -1604,7 +1603,7 @@ void level_map::depersist(lua_persist_reader* pReader) {
     }
 
     if (!pReader->read_stack_object()) return;
-    pNode->entities.next = static_cast<link_list*>(lua_touserdata(L, -1));
+    pNode->entities.next = luaT_testuserdata<animation_base>(L, -1, false);
     if (pNode->entities.next) {
       if (pNode->entities.next->prev != nullptr) {
         std::fprintf(stderr, "Warning: THMap linked-lists are corrupted.\n");
@@ -1614,7 +1613,8 @@ void level_map::depersist(lua_persist_reader* pReader) {
     lua_pop(L, 1);
 
     if (!pReader->read_stack_object()) return;
-    pNode->oEarlyEntities.next = static_cast<link_list*>(lua_touserdata(L, -1));
+    pNode->oEarlyEntities.next =
+        luaT_testuserdata<animation_base>(L, -1, false);
     if (pNode->oEarlyEntities.next) {
       if (pNode->oEarlyEntities.next->prev != nullptr) {
         std::fprintf(stderr, "Warning: THMap linked-lists are corrupted.\n");

CorsixTH/Src/th_lua.h
@@ -367,10 +367,10 @@ T* luaT_testuserdata(lua_State* L, int idx, int mt_idx, bool required = true) {
 }
 
 template <class T>
-T* luaT_testuserdata(lua_State* L, int idx = 1) {
+T* luaT_testuserdata(lua_State* L, int idx = 1, bool required = true) {
   int iMetaIndex = luaT_environindex;
   if (idx > 1) iMetaIndex = luaT_upvalueindex(idx - 1);
-  return luaT_testuserdata<T>(L, idx, iMetaIndex);
+  return luaT_testuserdata<T>(L, idx, iMetaIndex, required);
 }
 
 template <class T, int mt>

commit 777259614d05d1e88e691b7e58147f01322a444e

Fix Some depersisted Objects/Animations are not fully rendered #2826 (#2835)
A regression introduced in 5ff228ac21caeae6aba876728b9b36da9f0a1f5a.

luaT_testuserdata only works correctly if it is passed the correct
metatable reference, and the implementation we had assumed that was the
current environment table, but in the case of map loading it wasn't.

I wrote a replacement method for determining the class of the
animation_base userdata that depends on the __class_info stored in the
metatable.

There was an additional problem with persistence breaking any new saves:
it was using the void* of link_list rather than the class itself.
link_list was not polymorphic so I added the virtual destructor to fix
that and give it a vtable, then dynamic_cast to ensure the proper
pointer type was persisted.

CorsixTH/Src/persist_lua.cpp
@@ -30,7 +30,6 @@ SOFTWARE.
 #include <cstdlib>
 #include <cstring>
 #include <string>
-#include <vector>
 
 #include "th_lua.h"
 #ifdef _MSC_VER

CorsixTH/Src/th.h
@@ -30,7 +30,7 @@ SOFTWARE.
 class link_list {
  public:
   link_list();
-  ~link_list();
+  virtual ~link_list();
 
   link_list* prev;
   link_list* next;

CorsixTH/Src/th_gfx.cpp
@@ -1284,7 +1284,11 @@ void animation::persist(lua_persist_writer* pWriter) const {
 
   // Write the next chained thing
   lua_rawgeti(L, luaT_environindex, 2);
-  lua_pushlightuserdata(L, next);
+  void* np = dynamic_cast<animation*>(next);
+  if (np == nullptr) {
+    np = dynamic_cast<sprite_render_list*>(next);
+  }
+  lua_pushlightuserdata(L, np);
   lua_rawget(L, -2);
   pWriter->fast_write_stack_object(-1);
   lua_pop(L, 2);
@@ -1355,7 +1359,7 @@ void animation::depersist(lua_persist_reader* pReader) {
     // Read the chain
     if (!pReader->read_stack_object()) break;
 
-    next = luaT_testuserdata<animation_base>(L, -1, false);
+    next = luaT_toanimationbase(L, -1);
     if (next) next->prev = this;
     lua_pop(L, 1);
 
@@ -1789,7 +1793,11 @@ void sprite_render_list::persist(lua_persist_writer* pWriter) const {
 
   // Write the next chained thing
   lua_rawgeti(L, luaT_environindex, 2);
-  lua_pushlightuserdata(L, next);
+  void* np = dynamic_cast<animation*>(next);
+  if (np == nullptr) {
+    np = dynamic_cast<sprite_render_list*>(next);
+  }
+  lua_pushlightuserdata(L, np);
   lua_rawget(L, -2);
   pWriter->fast_write_stack_object(-1);
   lua_pop(L, 2);
@@ -1844,7 +1852,7 @@ void sprite_render_list::depersist(lua_persist_reader* pReader) {
     return;
   }
 
-  next = luaT_testuserdata<animation_base>(L, -1, false);
+  next = luaT_toanimationbase(L, -1);
   if (next) {
     next->prev = this;
   }

CorsixTH/Src/th_gfx.h
@@ -23,6 +23,8 @@ SOFTWARE.
 #ifndef CORSIX_TH_TH_GFX_H_
 #define CORSIX_TH_TH_GFX_H_
 
+#include <cstdio>
+#include <cstring>
 #include <map>
 #include <string>
 #include <vector>
@@ -660,7 +662,7 @@ class animation : public animation_base {
 class sprite_render_list : public animation_base {
  public:
   sprite_render_list();
-  ~sprite_render_list();
+  ~sprite_render_list() override;
 
   void tick();
   void draw(render_target* pCanvas, int iDestX, int iDestY);
@@ -709,16 +711,51 @@ class sprite_render_list : public animation_base {
   bool use_intermediate_buffer = false;
 };
 
-// Specialization of the 'luaT_testuserdata' template for the 'animation_base'
-// class.
-template <>
-inline animation_base* luaT_testuserdata<animation_base>(lua_State* L, int idx,
-                                                         bool required) {
-  animation_base* p = luaT_testuserdata<animation>(L, idx, false);
-  if (p == nullptr) {
-    p = luaT_testuserdata<sprite_render_list>(L, idx, required);
+// Find the appropriate subclass of animation_base for a given
+// userdata object.
+//
+// th_lua_internal.h defines our c++ binding class metatables:
+// metatable: { __index = method_table, ... }
+// method_table: { ... }
+// method_table metatable: { __class_name = class_name }
+inline animation_base* luaT_toanimationbase(lua_State* L, int idx) {
+  void* p = lua_touserdata(L, idx);
+  if (!p) {
+    return nullptr;
   }
-  return p;
+  if (lua_getmetatable(L, idx) == 0) {
+    std::printf("Warn: No metatable for animation_base userdata\n");
+    return nullptr;
+  }
+  lua_getfield(L, -1, "__index");
+  if (lua_type(L, -1) != LUA_TTABLE) {
+    lua_pop(L, 2);
+    std::printf("Warn: No __index field method table animation_base\n");
+    return nullptr;
+  }
+  if (lua_getmetatable(L, -1) == 0) {
+    lua_pop(L, 2);
+    std::printf("Warn: No metatable for method table of animation_base\n");
+    return nullptr;
+  }
+  lua_getfield(L, -1, "__class_name");
+  if (lua_type(L, -1) != LUA_TSTRING) {
+    lua_pop(L, 4);
+    std::printf("Warn: No __class_name field for animation_base\n");
+    return nullptr;
+  }
+  const char* class_name = lua_tostring(L, -1);
+  if (std::strcmp(class_name, "animation") == 0) {
+    lua_pop(L, 4);
+    return static_cast<animation*>(p);
+  } else if (std::strcmp(class_name, "spriteList") == 0) {
+    lua_pop(L, 4);
+    return static_cast<sprite_render_list*>(p);
+  } else {
+    std::printf("Warn: Unknown animation_base class name %s\n", class_name);
+    lua_pop(L, 4);
+    return nullptr;
+  }
 }
 
 #endif  // CORSIX_TH_TH_GFX_H_

CorsixTH/Src/th_map.cpp
@@ -1603,7 +1603,7 @@ void level_map::depersist(lua_persist_reader* pReader) {
     }
 
     if (!pReader->read_stack_object()) return;
-    pNode->entities.next = luaT_testuserdata<animation_base>(L, -1, false);
+    pNode->entities.next = luaT_toanimationbase(L, -1);
     if (pNode->entities.next) {
       if (pNode->entities.next->prev != nullptr) {
         std::fprintf(stderr, "Warning: THMap linked-lists are corrupted.\n");
@@ -1613,8 +1613,7 @@ void level_map::depersist(lua_persist_reader* pReader) {
     lua_pop(L, 1);
 
     if (!pReader->read_stack_object()) return;
-    pNode->oEarlyEntities.next =
-        luaT_testuserdata<animation_base>(L, -1, false);
+    pNode->oEarlyEntities.next = luaT_toanimationbase(L, -1);
     if (pNode->oEarlyEntities.next) {
       if (pNode->oEarlyEntities.next->prev != nullptr) {
         std::fprintf(stderr, "Warning: THMap linked-lists are corrupted.\n");