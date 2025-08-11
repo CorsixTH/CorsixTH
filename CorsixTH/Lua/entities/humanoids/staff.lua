c0a3a021e3493b75b17abe7772f1e3f7db334913
Artem Argus Gusakov <arg.amx@gmail.com>
2025-08-11 20:10:13 +0300

    disrupt service almost in any room fix

--- CorsixTH/Lua/entities/humanoids/staff.lua
+++ CorsixTH/Lua/entities/humanoids/staff.lua
@@ -534,9 +534,6 @@ function Staff:isIdle()
     return false
   end
 
-  if self.waiting_on_other_staff then
-    return false
-  end
   -- if they are using a door they are not idle, this stops doctors being considered for staff selection
   -- for rooms they have not completely left yet, fixes issue 810
   if self.user_of and self.user_of.object_type.id == "door" then
