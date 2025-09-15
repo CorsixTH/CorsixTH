3fc8cddd9cc32b8d81a75aa7ca62d9c46bdcd5b6
MandrakeHorse <142453312+MandrakeHorse@users.noreply.github.com>
2025-09-15 21:28:30 +0100

    forgot to close the string!

--- CorsixTH/Lua/config_finder.lua
+++ CorsixTH/Lua/config_finder.lua
@@ -399,13 +399,13 @@ local string_01 = [=[
 -- By default machine menu is shown in a bottom panel. If you would like the
 -- game to hide it change this option to false.
 --]=] .. '\n' ..
-'machine_menu_button = ' .. tostring(config_values.machine_menu_button) .. '\n' .. [=[]=]
+'machine_menu_button = ' .. tostring(config_values.machine_menu_button) .. '\n' .. [=[
 
 -------------------------------------------------------------------------------
 -- By default the entire screen will shake during earthquakes. If you would
 -- like the game to keep the screen stationary, change this option to false.
 --]=] .. '\n' ..
-'enable_screen_shake = ' .. tostring(config_values.enable_screen_shake) .. '\n' .. [=[
+'enable_screen_shake = ' .. tostring(config_values.enable_screen_shake) .. '\n' .. [=[]=]
 
 local string_02 = [=[
 
