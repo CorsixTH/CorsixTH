7ee2e5f0878ac3aa09ac4c377a21197e4f1e5cc3
Alberth <alberth289346@gmail.com>
2025-10-18 17:41:45 +0200

    Use drawing layer 7 for floating dollars

--- CorsixTH/Lua/utility.lua
+++ CorsixTH/Lua/utility.lua
@@ -228,7 +228,8 @@ DrawingLayers.AtomAnalyser = 3
 DrawingLayers.ReceptionistFacingUser = 3 -- Facing east or south.
 DrawingLayers.Entity = 4 -- All 'normal' animations.
 DrawingLayers.ReceptionistFacingAway = 5 -- Facing west or north.
--- Values 6 and 7 not used.
+-- Value 6 is not used.
+DrawingLayers.FloatingDollars = 7
 DrawingLayers.EastSideObject = 8
 DrawingLayers.SouthSideObject = 9
 
