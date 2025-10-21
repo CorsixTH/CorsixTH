9254cc66f2092855fef04e2c039d248a3744ae98
Marcassin <quentin.sanglier@gmail.com>
2025-10-21 20:51:52 +0200

    handle right click in edit room

--- CorsixTH/Lua/dialogs/edit_room.lua
+++ CorsixTH/Lua/dialogs/edit_room.lua
@@ -960,8 +960,7 @@ function UIEditRoom:enterDoorPhase()
     end
   end
 
-  self.desc_text = _S.place_objects_window.place_door
-  self.confirm_button:enable(false) -- Confirmation is via placing door
+  self:setDoorPhasePanelInfo()
 
   -- Change the floor tiles to opaque blue
   local map = self.ui.app.map.th
@@ -981,6 +980,11 @@ function UIEditRoom:enterDoorPhase()
     walls[x][y] = wall
   end
   self.blueprint_wall_anims = walls
+end
+
+function UIEditRoom:setDoorPhasePanelInfo()
+  self.desc_text = _S.place_objects_window.place_door
+  self.confirm_button:enable(false) -- Confirmation is via placing door
 end
 
 function UIEditRoom:enterWindowsPhase()
@@ -1072,32 +1076,110 @@ function UIEditRoom:draw(canvas, ...)
   UIPlaceObjects.draw(self, canvas, ...)
 end
 
+--25 north wall, west
+--26 north wall, east
+--27 east wall, south
+--28 east wall, north
+--29 south wall, west
+--30 south wall, east
+--31 west wall, south
+--32 west wall, north
+-- single door blue print values matching TH
+local door_floor_blueprint_markers = {
+  north = 26,
+  east = 27,
+  south = 30,
+  west = 31
+}
+
+local window_floor_blueprint_markers = {
+  north = 33,
+  east = 34,
+  south = 35,
+  west = 36,
+}
+
+
+function UIEditRoom:onLeftButtonDown(button, x, y)
+  if self.phase == "walls" then
+    if 0 <= x and x < self.width and 0 <= y and y < self.height then -- luacheck: ignore 542
+    else
+      local mouse_x, mouse_y = self.ui:ScreenToWorld(self.x + x, self.y + y)
+      self.mouse_down_x = math.floor(mouse_x)
+      self.mouse_down_y = math.floor(mouse_y)
+      if self.move_rect then
+        self.move_rect_x = self.mouse_down_x - self.blueprint_rect.x
+        self.move_rect_y = self.mouse_down_y - self.blueprint_rect.y
+      elseif not self.resize_rect then
+        self:setBlueprintRect(self.mouse_down_x, self.mouse_down_y, 1, 1)
+      end
+    end
+  elseif self.phase == "door" then
+    if self.blueprint_door.valid then
+      self.ui:playSound("buildclk.wav")
+      self:confirm(true)
+    else
+      self.ui:tutorialStep(3, 9, 10)
+    end
+  elseif self.phase == "windows" then
+    self:placeWindowBlueprint()
+  end
+end
+
+-- Remove door or window
+function UIEditRoom:onRightButtonDown(button, x, y)
+  if self.phase == "door" or self.phase == "windows" then
+    local map = TheApp.map.th
+    -- local cell_x, cell_y = self.ui:ScreenToWorld(self.x + x, self.y + y)
+    -- cell_x, cell_y = math.floor(cell_x), math.floor(cell_y)
+
+    local cellx, celly, wall_dir = self:screenToWall(self.x + x, self.y + y)
+    print(cellx, celly, wall_dir)
+    if not cellx then
+      return
+    end
+
+    local cell_flag = map:getCell(cellx, celly, 4)
+    print("flag=", cell_flag, " is door ? ", cell_flag == door_floor_blueprint_markers[wall_dir])
+    local new_anim = 120
+    if cell_flag == door_floor_blueprint_markers[wall_dir] then
+      self.phase = "door" -- Changing phase to force the door to be the next item to add
+      self:setDoorPhasePanelInfo()
+      -- if self.blueprint_door and self.blueprint_door.old_anim == 130 then
+      --   new_anim
+      -- end
+    end
+
+    local wall_x, wall_y = cellx, celly
+
+    local direction_flag = ( wall_dir == "south" or wall_dir == "north") and 0 or 1
+    if wall_dir == "south" then
+      wall_y = celly + 1
+    elseif wall_dir == "east" then
+      wall_x = cellx + 1
+    end
+
+    local anim = self.blueprint_wall_anims[wall_x][wall_y]
+    if anim then
+      -- reset tile to wall/basic floor
+      map:setCell(cellx, celly, 4, 24)
+      local old_flag = anim:getFlag()
+      print("set animation 120 - ", direction_flag, " for ", wall_x, wall_y)
+      anim:setAnimation(self.anims, 120, direction_flag)
+      anim:setTag(nil)
+    else
+      print("Anim empty")
+    end
+  end
+
+end
+
 function UIEditRoom:onMouseDown(button, x, y)
   if self.world.user_actions_allowed and not self.confirm_dialog_open then
     if button == "left" then
-      if self.phase == "walls" then
-        if 0 <= x and x < self.width and 0 <= y and y < self.height then -- luacheck: ignore 542
-        else
-          local mouse_x, mouse_y = self.ui:ScreenToWorld(self.x + x, self.y + y)
-          self.mouse_down_x = math.floor(mouse_x)
-          self.mouse_down_y = math.floor(mouse_y)
-          if self.move_rect then
-            self.move_rect_x = self.mouse_down_x - self.blueprint_rect.x
-            self.move_rect_y = self.mouse_down_y - self.blueprint_rect.y
-          elseif not self.resize_rect then
-            self:setBlueprintRect(self.mouse_down_x, self.mouse_down_y, 1, 1)
-          end
-        end
-      elseif self.phase == "door" then
-        if self.blueprint_door.valid then
-          self.ui:playSound("buildclk.wav")
-          self:confirm(true)
-        else
-          self.ui:tutorialStep(3, 9, 10)
-        end
-      elseif self.phase == "windows" then
-        self:placeWindowBlueprint()
-      end
+      self:onLeftButtonDown(button, x, y)
+    elseif button == "right" then
+      self:onRightButtonDown(button, x, y)
     end
   end
   return UIPlaceObjects.onMouseDown(self, button, x, y) or true
@@ -1165,29 +1247,6 @@ function UIEditRoom:setBlueprintRect(x, y, w, h)
   rect.w = w
   rect.h = h
 end
-
---25 north wall, west
---26 north wall, east
---27 east wall, south
---28 east wall, north
---29 south wall, west
---30 south wall, east
---31 west wall, south
---32 west wall, north
--- single door blue print values matching TH
-local door_floor_blueprint_markers = {
-  north = 26,
-  east = 27,
-  south = 30,
-  west = 31
-}
-
-local window_floor_blueprint_markers = {
-  north = 33,
-  east = 34,
-  south = 35,
-  west = 36,
-}
 
 --! Check walls for having room for the door
 --!param x (int) X tile position of the door.
