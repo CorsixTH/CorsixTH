a8ec73e5859df48f393ab3296e16609dccda5740
lewri <lewri@users.noreply.github.com>
2025-10-29 00:13:32 +0000

    Don't assume staff_member_set for all rooms (#3097)
    * Don't assume staff_member_set for all rooms

--- CorsixTH/Lua/app.lua
+++ CorsixTH/Lua/app.lua
@@ -28,7 +28,7 @@ local SDL = require("sdl")
 -- and add compatibility code in afterLoad functions
 -- Recommended: Also replace/Update the summary comment
 
-local SAVEGAME_VERSION = 234 -- Overhaul training
+local SAVEGAME_VERSION = 235 -- Fix staff_member_set applying to all rooms
 
 class "App"
 
