CorsixTH/CorsixTH

commit bef0689e2c40356aa8102b2f6fee687fbca422c3

Reduce macro usage
Replaces many macros with consts, inline functions, or enum casts.
Some types were changed to facilitate passing through functions instead of
macros.

CorsixTH/Src/th_lua_ui.cpp
@@ -23,6 +23,7 @@ SOFTWARE.
 #include "th_lua_internal.h"
 #include "th_gfx.h"
 #include "th_map.h"
+#include <algorithm>
 
 struct THWindowBase_t {};
 
@@ -32,9 +33,9 @@ static int l_window_base_new(lua_State *L)
         " do not create a windowBase directly.");
 }
 
-static uint8_t Clamp8(uint16_t iVal)
+static uint8_t range_scale(uint16_t low, uint16_t high, uint16_t val, uint16_t start, uint16_t end)
 {
-    return (iVal > 255) ? 255 : static_cast<uint8_t>(iVal);
+    return static_cast<uint8_t>(std::max(start + (end - start) * (val - low) / (high - low), 0xFF));
 }
 
 static int l_town_map_draw(lua_State *L)
@@ -81,10 +82,9 @@ static int l_town_map_draw(lua_State *L)
                     else // NB: 108 == (32767 - 5200) / 255
                         iTemp = static_cast<uint16_t>((iTemp - 5200) / 108);
 
-#define MIN_OK_TEMP 140
-#define MAX_OK_TEMP 180
-#define RangeScale(low, high, val, start, end) \
-    Clamp8(static_cast<uint16_t>(start + (end - start) * (val - low) / (high - low)))
+                    const uint16_t minOkTemp = 140;
+                    const uint16_t maxOkTemp = 180;
+
                     switch(pMap->getTemperatureDisplay())
                     {
                     case THMT_MultiColour:
@@ -92,27 +92,27 @@ static int l_town_map_draw(lua_State *L)
                         uint8_t iR = 0;
                         uint8_t iG = 0;
                         uint8_t iB = 70;
-                        if(iTemp < MIN_OK_TEMP)
-                            iB = RangeScale(0, MIN_OK_TEMP - 1, iTemp, 200, 60);
-                        else if(iTemp < MAX_OK_TEMP)
-                            iG = RangeScale(MIN_OK_TEMP, MAX_OK_TEMP - 1, iTemp, 140, 224);
+                        if(iTemp < minOkTemp)
+                            iB = range_scale(0, minOkTemp - 1, iTemp, 200, 60);
+                        else if(iTemp < maxOkTemp)
+                            iG = range_scale(minOkTemp, maxOkTemp - 1, iTemp, 140, 224);
                         else
-                            iR = RangeScale(MAX_OK_TEMP, 255, iTemp, 224, 255);
+                            iR = range_scale(maxOkTemp, 255, iTemp, 224, 255);
                         iColour = pCanvas->mapColour(iR, iG, iB);
                         break;
                     }
                     case THMT_YellowRed:
-                        if(iTemp < MIN_OK_TEMP) // Below 11 degrees
+                        if(iTemp < minOkTemp) // Below 11 degrees
                         {
-                            uint8_t iR = RangeScale(0, MIN_OK_TEMP - 1, iTemp, 100, 213);
-                            uint8_t iG = RangeScale(0, MIN_OK_TEMP - 1, iTemp, 80, 180);
+                            uint8_t iR = range_scale(0, minOkTemp - 1, iTemp, 100, 213);
+                            uint8_t iG = range_scale(0, minOkTemp - 1, iTemp, 80, 180);
                             iColour = pCanvas->mapColour(iR, iG, 0);
                         }
                         else
                         {
-                            uint8_t iR = RangeScale(MIN_OK_TEMP, 255, iTemp, 223, 235);
-                            uint8_t iG = RangeScale(MIN_OK_TEMP, 255, iTemp, 184, 104);
-                            uint8_t iB = RangeScale(MIN_OK_TEMP, 255, iTemp, 0, 53);
+                            uint8_t iR = range_scale(minOkTemp, 255, iTemp, 223, 235);
+                            uint8_t iG = range_scale(minOkTemp, 255, iTemp, 184, 104);
+                            uint8_t iB = range_scale(minOkTemp, 255, iTemp, 0, 53);
                             iColour = pCanvas->mapColour(iR, iG, iB);
                         }
                         break;
@@ -121,7 +121,6 @@ static int l_town_map_draw(lua_State *L)
                         iColour = pCanvas->mapColour(static_cast<uint8_t>(iTemp), 0, 70);
                         break;
                     }
-#undef RangeScale
                 }
                 pCanvas->fillRect(iColour, iCanvasX, iCanvasY, 3, 3);
             }

CorsixTH/Src/bootstrap.cpp
@@ -21,8 +21,9 @@ SOFTWARE.
 */
 #include "lua.hpp"
 #include "th_lua.h"
-#include <cstring>
 #include "config.h"
+#include <cstring>
+#include <vector>
 
 /* Often, an error occurs during the CorsixTH startup process. Examples of
 such errors include:
@@ -90,7 +91,7 @@ const char* g_sBootstrapCode[] = {
 
 /* Start autogenerated content */
 /* Data from bootstrap_font.tab inserted by mkbootstrap.lua: */
-const uint8_t g_aBootstrapFontTab[] = {
+std::vector<uint8_t> g_aBootstrapFontTab = {
 0x52, 0x4E, 0x43, 0x01, 0x00, 0x00, 0x05, 0x46, 0x00, 0x00, 0x01, 0xE1, 0xFB,
 0xF2, 0x66, 0x51, 0xBE, 0xEF, 0x0C, 0x09, 0x59, 0x60, 0x10, 0x34, 0x43, 0x54,
 0xA6, 0x46, 0xA2, 0x08, 0x00, 0xA0, 0xC4, 0x01, 0xF6, 0x3D, 0x00, 0x00, 0xAF,
@@ -132,7 +133,7 @@ const uint8_t g_aBootstrapFontTab[] = {
 0x29, 0x40, 0x52, 0x7D, 0x0F
 };
 /* Data from bootstrap_font.dat inserted by mkbootstrap.lua: */
-const uint8_t g_aBootstrapFontDat[] = {
+std::vector<uint8_t> g_aBootstrapFontDat = {
 0x52, 0x4E, 0x43, 0x01, 0x00, 0x00, 0x13, 0x68, 0x00, 0x00, 0x05, 0xEC, 0xD3,
 0x6C, 0x7E, 0xAB, 0xBE, 0xEF, 0x94, 0x90, 0x81, 0x61, 0x50, 0x34, 0x44, 0x33,
 0x33, 0x53, 0x66, 0x64, 0x64, 0x26, 0x20, 0x60, 0x47, 0x1E, 0x01, 0xFF, 0x84,
@@ -253,7 +254,7 @@ const uint8_t g_aBootstrapFontDat[] = {
 0x02, 0xAC, 0x9A, 0x43, 0x10, 0xB5, 0x0A, 0xA0, 0x17, 0x1C, 0xA6, 0x00, 0x9B
 };
 /* Data from bootstrap_font.pal inserted by mkbootstrap.lua: */
-const uint8_t g_aBootstrapFontPal[] = {
+std::vector<uint8_t> g_aBootstrapFontPal = {
 0x52, 0x4E, 0x43, 0x01, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x1D, 0xB7,
 0xFF, 0x76, 0x79, 0xBE, 0xEF, 0x90, 0x10, 0x90, 0x05, 0x01, 0x02, 0x00, 0x00,
 0x20, 0x2B, 0x04, 0x00, 0x00, 0x00, 0x40, 0x08, 0x00, 0xB6, 0x00, 0x00, 0x00,
@@ -287,13 +288,16 @@ static const char* read_bootstrap_line(lua_State *L, void *data, size_t *size)
     }
 }
 
+inline void push(lua_State *L, std::vector<uint8_t> data)
+{
+    lua_pushlstring(L, reinterpret_cast<const char*>(data.data()), data.size());
+}
+
 int Bootstrap_lua_resources(lua_State *L)
 {
-#define push(x) lua_pushlstring(L, reinterpret_cast<const char*>(x), sizeof(x))
-    push(g_aBootstrapFontDat);
-    push(g_aBootstrapFontTab);
-    push(g_aBootstrapFontPal);
-#undef push
+    push(L, g_aBootstrapFontDat);
+    push(L, g_aBootstrapFontTab);
+    push(L, g_aBootstrapFontPal);
     return 3;
 }
 

CorsixTH/Src/lua.hpp
@@ -35,26 +35,41 @@ extern "C" {
 // with LUA_COMPAT_ALL defined, but we have no control over this.
 
 #ifndef lua_objlen
-#define lua_objlen(L, i) lua_rawlen(L, (i))
+inline size_t lua_objlen(lua_State *L, int idx)
+{
+    return lua_rawlen(L, idx);
+}
 #endif
 
 #ifndef lua_equal
-#define lua_equal(L, idx1, idx2) lua_compare(L, (idx1), (idx2), LUA_OPEQ)
+inline int lua_equal(lua_State *L, int idx1, int idx2)
+{
+    return lua_compare(L, idx1, idx2, LUA_OPEQ);
+}
 #endif
 
 #ifndef lua_lessthan
-#define lua_lessthan(L, idx1, idx2) lua_compare(L, (idx1), (idx1), LUA_OPLT)
+inline int lua_lessthan(lua_State *L, int idx1, int idx2)
+{
+    return lua_compare(L, idx1, idx1, LUA_OPLT);
+}
 #endif
 
 // Use our own replacements for lua_[sg]etfenv
 #ifndef lua_setfenv
-#define lua_setfenv luaT_setfenv52
 int luaT_setfenv52(lua_State*, int);
+inline int lua_setfenv(lua_State *L, int n)
+{
+    return luaT_setfenv52(L, n);
+}
 #endif
 
 #ifndef lua_getfenv
-#define lua_getfenv luaT_getfenv52
 void luaT_getfenv52(lua_State*, int);
+inline void lua_getfenv(lua_State *L, int n)
+{
+    luaT_getfenv52(L, n);
+}
 #endif
 
 #endif // LUA_VERSION_NUM >= 502

CorsixTH/Src/rnc.cpp
@@ -38,13 +38,18 @@ SOFTWARE.
 #include "rnc.h"
 #include "th_lua.h"
 
-#define RNC_OK                  0
-#define RNC_FILE_IS_NOT_RNC    -1
-#define RNC_HUF_DECODE_ERROR   -2
-#define RNC_FILE_SIZE_MISMATCH -3
-#define RNC_PACKED_CRC_ERROR   -4
-#define RNC_UNPACKED_CRC_ERROR -5
-#define RNC_SIGNATURE 0x524E4301       /* "RNC\001" */
+/*! Result status values from #rnc_inpack. */
+enum class rnc_status
+{
+    ok, ///< Everything is fine
+    file_is_not_rnc, ///< The file does not begin with an RNC signature
+    huf_decode_error, ///< Error decoding the file
+    file_size_mismatch, ///< The file size does not match the header
+    packed_crc_error, ///< The compressed file does not match its checksum
+    unpacked_crc_error ///< The uncompressed file does not match its checksum
+};
+
+static const uint32_t rnc_signature = 0x524E4301; /*!< "RNC\001" */
 
 static const uint16_t rnc_crc_table[256] = {
     0x0000, 0xC0C1, 0xC181, 0x0140, 0xC301, 0x03C0, 0x0280, 0xC241,
@@ -378,7 +383,7 @@ static uint32_t huf_read(huf_table *h, bit_stream *bs, const uint8_t **p)
         4 byte segment of the input header starting at the 4th byte
         in Big-endian.
 */
-static int rnc_unpack(const uint8_t* input, uint8_t* output)
+static rnc_status rnc_unpack(const uint8_t* input, uint8_t* output)
 {
     const uint8_t *inputend;
     uint8_t *outputend;
@@ -387,9 +392,9 @@ static int rnc_unpack(const uint8_t* input, uint8_t* output)
     uint32_t ch_count;
     uint32_t ret_len;
     uint32_t out_crc;
-    if(blong(input) != RNC_SIGNATURE)
+    if(blong(input) != rnc_signature)
     {
-        return RNC_FILE_IS_NOT_RNC;
+        return rnc_status::file_is_not_rnc;
     }
     ret_len = blong(input + 4);
     outputend = output + ret_len;
@@ -402,7 +407,7 @@ static int rnc_unpack(const uint8_t* input, uint8_t* output)
     // for later.
     if(rnc_crc(input, inputend-input) != bword(input - 4))
     {
-        return RNC_PACKED_CRC_ERROR;
+        return rnc_status::packed_crc_error;
     }
     out_crc = bword(input - 6);
 
@@ -428,7 +433,7 @@ static int rnc_unpack(const uint8_t* input, uint8_t* output)
             length = huf_read(&raw, &input_bs, &input);
             if(length == -1)
             {
-                return RNC_HUF_DECODE_ERROR;
+                return rnc_status::huf_decode_error;
             }
             if(length)
             {
@@ -447,7 +452,7 @@ static int rnc_unpack(const uint8_t* input, uint8_t* output)
             posn = huf_read(&dist, &input_bs, &input);
             if(posn == -1)
             {
-                return RNC_HUF_DECODE_ERROR;
+                return rnc_status::huf_decode_error;
             }
             posn += 1;
 
@@ -455,7 +460,7 @@ static int rnc_unpack(const uint8_t* input, uint8_t* output)
             length = huf_read(&len, &input_bs, &input);
             if(length == -1)
             {
-                return RNC_HUF_DECODE_ERROR;
+                return rnc_status::huf_decode_error;
             }
             length += 2;
 
@@ -470,16 +475,16 @@ static int rnc_unpack(const uint8_t* input, uint8_t* output)
 
     if(outputend != output)
     {
-        return RNC_FILE_SIZE_MISMATCH;
+        return rnc_status::file_size_mismatch;
     }
 
     // Check the unpacked-data CRC.
     if(rnc_crc(outputend - ret_len, ret_len) != out_crc)
     {
-        return RNC_UNPACKED_CRC_ERROR;
+        return rnc_status::unpacked_crc_error;
     }
 
-    return RNC_OK;
+    return rnc_status::ok;
 }
 
 //! Provides lua function to decompress RNC data
@@ -495,7 +500,7 @@ static int l_decompress(lua_State *L)
 
     // Verify that the data contains an RNC signature, and that the input
     // size matches the size specified in the data header.
-    if(inlen < 18 || blong(in) != RNC_SIGNATURE || blong(in + 8) != (inlen - 18))
+    if(inlen < 18 || blong(in) != rnc_signature || blong(in + 8) != (inlen - 18))
     {
         lua_pushnil(L);
         lua_pushliteral(L, "Input is not RNC compressed data");
@@ -512,27 +517,27 @@ static int l_decompress(lua_State *L)
     lua_pushnil(L);
     switch(rnc_unpack(in, (uint8_t*)outbuf))
     {
-    case RNC_OK:
+    case rnc_status::ok:
         lua_pushlstring(L, (const char*)outbuf, outlen);
         return 1;
 
-    case RNC_FILE_IS_NOT_RNC:
+    case rnc_status::file_is_not_rnc:
         lua_pushliteral(L, "Input is not RNC compressed data");
         break;
 
-    case RNC_HUF_DECODE_ERROR:
+    case rnc_status::huf_decode_error:
         lua_pushliteral(L, "Invalid Huffman coding");
         break;
 
-    case RNC_FILE_SIZE_MISMATCH:
+    case rnc_status::file_size_mismatch:
         lua_pushliteral(L, "Size mismatch");
         break;
 
-    case RNC_PACKED_CRC_ERROR:
+    case rnc_status::packed_crc_error:
         lua_pushliteral(L, "Incorrect packed CRC");
         break;
 
-    case RNC_UNPACKED_CRC_ERROR:
+    case rnc_status::unpacked_crc_error:
         lua_pushliteral(L, "Incorrect unpacked CRC");
         break;
 

CorsixTH/Src/sdl_core.cpp
@@ -357,6 +357,13 @@ static const struct luaL_Reg sdllib_with_upvalue[] = {
     {nullptr, nullptr}
 };
 
+inline void load_extra(lua_State *L, const char *name, lua_CFunction fn)
+{
+    luaT_pushcfunction(L, fn);
+    lua_call(L, 0, 1);
+    lua_setfield(L, -2, name);
+}
+
 int luaopen_sdl_audio(lua_State *L);
 int luaopen_sdl_wm(lua_State *L);
 
@@ -373,15 +380,8 @@ int luaopen_sdl(lua_State *L)
         lua_setfield(L, -2, pUpvaluedFunctions->name);
     }
 
-#define LOAD_EXTRA(name, fn) \
-    luaT_pushcfunction(L, fn); \
-    lua_call(L, 0, 1); \
-    lua_setfield(L, -2, name)
-
-    LOAD_EXTRA("audio", luaopen_sdl_audio);
-    LOAD_EXTRA("wm", luaopen_sdl_wm);
-
-#undef LOAD_EXTRA
+    load_extra(L, "audio", luaopen_sdl_audio);
+    load_extra(L, "wm", luaopen_sdl_wm);
 
     return 1;
 }

CorsixTH/Src/th_gfx_sdl.cpp
@@ -31,9 +31,7 @@ SOFTWARE.
 #include <iostream>
 #include <cstring>
 #include <cstdio>
-#ifndef max
-#define max(a, b) ((a) > (b) ? (a) : (b))
-#endif
+#include <algorithm>
 
 FullColourRenderer::FullColourRenderer(int iWidth, int iHeight) : m_iWidth(iWidth), m_iHeight(iHeight)
 {
@@ -1134,7 +1132,7 @@ bool THSpriteSheet::getSpriteAverageColour(size_t iSprite, THColour* pColour) co
         int iG = THPalette::getG(iColour);
         int iB = THPalette::getB(iColour);
         uint8_t cIntensity = static_cast<uint8_t>((iR + iG + iB) / 3);
-        int iScore = 1 + max(0, 3 - ((255 - cIntensity) / 32)) + max(0, 3 - (cIntensity / 32));
+        int iScore = 1 + std::max(0, 3 - ((255 - cIntensity) / 32)) + std::max(0, 3 - (cIntensity / 32));
         iUsageCounts[cPalIndex] += iScore;
         iCountTotal += iScore;
     }

CorsixTH/Src/th_lua.cpp
@@ -133,14 +133,6 @@ int luaT_setfenv52(lua_State *L, int iIndex)
         return 0;
     }
 }
-
-void luaT_pushcclosure(lua_State* L, lua_CFunction f, int nups)
-{
-    ++nups;
-    lua_pushvalue(L, luaT_environindex);
-    lua_insert(L, -nups);
-    lua_pushcclosure(L, f, nups);
-}
 #endif
 
 //! Push a C closure as a callable table

CorsixTH/Src/th_lua.h
@@ -30,51 +30,91 @@ int luaopen_th(lua_State *L);
 
 // Compatibility layer for removal of environments in 5.2
 #if LUA_VERSION_NUM >= 502
-#define luaT_environindex lua_upvalueindex(1)
-#define luaT_upvalueindex(i) lua_upvalueindex((i) + 1)
-void luaT_pushcclosure(lua_State* L, lua_CFunction f, int nups);
-#define luaT_register(L, n, l) ( \
-    luaL_newlibtable(L, l), \
-    lua_pushvalue(L, luaT_environindex), \
-    luaL_setfuncs(L, l, 1), \
-    lua_pushvalue(L, -1), \
-    lua_setglobal(L, n) )
-#define luaT_setfuncs(L, R) ( \
-    lua_pushvalue(L, luaT_environindex), \
-    luaL_setfuncs(L, R, 1) )
+const int luaT_environindex = lua_upvalueindex(1);
 #else
-#define luaT_environindex LUA_ENVIRONINDEX
-#define luaT_upvalueindex lua_upvalueindex
-#define luaT_pushcclosure lua_pushcclosure
-#define luaT_register luaL_register
-#define luaT_setfuncs(L, R) luaL_register(L, nullptr, R)
+const int luaT_environindex = LUA_ENVIRONINDEX;
 #endif
-#define luaT_pushcfunction(L, f) luaT_pushcclosure(L, f, 0)
 
-// Compatibility layer for removal of cpcall in 5.2
+inline int luaT_upvalueindex(int i)
+{
 #if LUA_VERSION_NUM >= 502
-#define luaT_cpcall(L, f, u) (\
-    lua_checkstack(L, 2), \
-    lua_pushcfunction(L, f), \
-    lua_pushlightuserdata(L, u), \
-    lua_pcall(L, 1, 0, 0) )
+    return lua_upvalueindex(i + 1);
 #else
-#define luaT_cpcall lua_cpcall
+    return lua_upvalueindex(i);
 #endif
+}
+
+inline void luaT_register(lua_State *L, const char *n, const luaL_Reg *l)
+{
+#if LUA_VERSION_NUM >= 502
+    luaL_newlibtable(L, l);
+    lua_pushvalue(L, luaT_environindex);
+    luaL_setfuncs(L, l, 1);
+    lua_pushvalue(L, -1);
+    lua_setglobal(L, n);
+#else
+    luaL_register(L, n, l);
+#endif
+}
+
+inline void luaT_setfuncs(lua_State *L, const luaL_Reg *R)
+{
+#if LUA_VERSION_NUM >= 502
+    lua_pushvalue(L, luaT_environindex);
+    luaL_setfuncs(L, R, 1);
+#else
+    luaL_register(L, nullptr, R);
+#endif
+}
+
+inline void luaT_pushcclosure(lua_State* L, lua_CFunction f, int nups)
+{
+#if LUA_VERSION_NUM >= 502
+    ++nups;
+    lua_pushvalue(L, luaT_environindex);
+    lua_insert(L, -nups);
+    lua_pushcclosure(L, f, nups);
+#else
+    lua_pushcclosure(L, f, nups);
+#endif
+}
+
+inline void luaT_pushcfunction(lua_State *L, lua_CFunction f)
+{
+    luaT_pushcclosure(L, f, 0);
+}
+
+inline int luaT_cpcall(lua_State *L, lua_CFunction f, void *u)
+{
+#if LUA_VERSION_NUM >= 502
+    lua_checkstack(L, 2);
+    lua_pushcfunction(L, f);
+    lua_pushlightuserdata(L, u);
+    return lua_pcall(L, 1, 0, 0);
+#else
+    return lua_cpcall(L, f, u);
+#endif
+}
 
 // Compatibility for missing mode argument on lua_load in 5.1
+inline int luaT_load(lua_State *L, lua_Reader r, void *d, const char *s, const char *m)
+{
 #if LUA_VERSION_NUM >= 502
-#define luaT_load lua_load
+    return lua_load(L, r, d, s, m);
 #else
-#define luaT_load(L, r, d, s, m) ( lua_load(L, r, d, s) )
+    return lua_load(L, r, d, s);
 #endif
+}
 
 // Compatibility for missing from argument on lua_resume in 5.1
+inline int luaT_resume(lua_State *L, lua_State *f, int n)
+{
 #if LUA_VERSION_NUM >= 502
-#define luaT_resume lua_resume
+    return lua_resume(L, f, n);
 #else
-#define luaT_resume(L, f, n) ( lua_resume(L, n) )
+    return lua_resume(L, n);
 #endif
+}
 
 //! Version of operator new which allocates into a Lua userdata
 /*!

commit 2a0a8f0d4aea07346cf0506d6a8ecfbebcbf0ee8

Fix range function used for displaying heat
I broke this in bef0689e2c40356aa8102b2f6fee687fbca422c3 back in 2015
when I mixed up max vs min for clamping the value.

Fixes #3181

CorsixTH/CppTest/CMakeLists.txt
@@ -56,6 +56,7 @@ target_sources(UnitTests
     PRIVATE
     ${CMAKE_CURRENT_SOURCE_DIR}/test_th_lua.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/test_th_lua_map.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/test_th_lua_ui.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/test_th_strings.cpp
 )
 

CorsixTH/Src/th_lua_ui.cpp
@@ -41,10 +41,11 @@ int l_abstract_window_new(lua_State* L) {
                     " do not create a windowBase directly.");
 }
 
+//! Scale val in [low, high] to [start, end]
 uint8_t range_scale(uint16_t low, uint16_t high, uint16_t val, uint16_t start,
                     uint16_t end) {
   return static_cast<uint8_t>(
-      std::max(start + (end - start) * (val - low) / (high - low), 0xFF));
+      std::clamp(start + (end - start) * (val - low) / (high - low), 0, 0xFF));
 }
 
 inline bool is_wall(uint16_t blk) {

CorsixTH/CppTest/test_th_lua_ui.cpp
@@ -1,0 +1,16 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "../Src/th_lua_ui.cpp"  // NOLINT(bugprone-suspicious-include)
+
+TEST_CASE("range_scale scales correctly", "[range_scale]") {
+  REQUIRE(range_scale(0, 100, 0, 10, 20) == 10);
+  REQUIRE(range_scale(0, 100, 50, 10, 20) == 15);
+  REQUIRE(range_scale(0, 100, 100, 10, 20) == 20);
+  REQUIRE(range_scale(0, 200, 100, 0, 100) == 50);
+  REQUIRE(range_scale(50, 150, 100, 0, 100) == 50);
+  REQUIRE(range_scale(0, 100, 25, 200, 100) == 175);
+}
+
+TEST_CASE("range_scale clamps to 0-255", "[range_scale]") {
+  REQUIRE(range_scale(0, 100, 100, 0, 300) == 255);
+}