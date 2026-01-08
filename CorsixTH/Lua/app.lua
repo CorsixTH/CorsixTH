9709c48ae0338ff55c1cd752bc053fa12317e385
Stephen E. Baker <baker.stephen.e@gmail.com>
2026-01-07 23:48:11 -0500

    Eliminate the new reserve word global
    Since global is a reserve word it is not valid as a lua identifier
    according to the reference manual. This meant that I could not reference
    the old global field to rename it. I computed it based on the algorithm
    used in the research_department dialog when the values are changed.
    
    For afterLoad we reference global by string literal. This works and has
    worked in every version of lua back to 5.1 with any reserve word so it's
    unlikely to break.

--- CorsixTH/Lua/app.lua
+++ CorsixTH/Lua/app.lua
@@ -28,7 +28,7 @@ local SDL = require("sdl")
 -- and add compatibility code in afterLoad functions
 -- Recommended: Also replace/Update the summary comment
 
-local SAVEGAME_VERSION = 237 -- Scale Fixes
+local SAVEGAME_VERSION = 238 -- Lua 5.5
 
 class "App"
 
