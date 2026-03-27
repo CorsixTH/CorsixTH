387c8a7284aaeb347bd876a80700084c1bc9f1e1
Matroftt <91145555+Matroftt@users.noreply.github.com>
2026-03-28 00:12:16 +0300

    Remove typo
    Co-authored-by: Toby <mrtobylane@gmail.com>

--- CorsixTH/Lua/app.lua
+++ CorsixTH/Lua/app.lua
@@ -28,7 +28,7 @@ local SDL = require("sdl")
 -- and add compatibility code in afterLoad functions
 -- Recommended: Also replace/Update the summary comment
 
-local SAVEGAME_VERSION = 242 -- Machine menu enchantments
+local SAVEGAME_VERSION = 242 -- Machine menu enhancements
 
 class "App"
 
