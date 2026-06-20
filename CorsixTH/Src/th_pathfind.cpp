934ec9bdef25a2ca893978db2201719ca7ae04a8
Alberth <alberth289346@gmail.com>
2026-06-20 11:03:43 +0200

    Forbid idling at tiles to avoid.

--- CorsixTH/Src/th_pathfind.cpp
+++ CorsixTH/Src/th_pathfind.cpp
@@ -304,8 +304,8 @@ bool idle_tile_finder::find_idle_tile(const level_map* pMap, int iStartX,
     map_tile_flags flags = node_tile->flags;
 
     bool correct_parcel = parcelId == 0 || parcelId == node_tile->iParcelId;
-    if (!flags.do_not_idle && flags.passable && flags.hospital &&
-        correct_parcel) {
+    if (!flags.do_not_idle !flags.avoid_tile && flags.passable
+        && flags.hospital && correct_parcel) {
       // Try to delay returning an idle tile until we find the Nth tile.
       if (iN == 0) {
         parent->destination = pNode;
