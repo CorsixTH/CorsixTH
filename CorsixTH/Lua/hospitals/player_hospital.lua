cf2c94357e48c20a611bdec6cf74e55d16007a4e
Toby <mrtobylane@gmail.com>
2026-08-13 18:38:54 +0100

    Update CorsixTH/Lua/hospitals/player_hospital.lua

--- CorsixTH/Lua/hospitals/player_hospital.lua
+++ CorsixTH/Lua/hospitals/player_hospital.lua
@@ -40,9 +40,9 @@ function PlayerHospital:PlayerHospital(world, avail_rooms, name)
 
     sitting_ratios = {}, -- Measurements of recent sitting/standing ratios.
     sitting_index = 1 -- Next entry in 'sitting_ratios' to update.
-    no_gp_office = nil -- Whether advice was given about patients and doctors waiting for a GP's office
-    no_doctor_no_gp_office = nill -- Whether advice was given about patients waiting for a doctor and a GP's office
-    last_patient_not_paying_month = nil -- Whether advice was given about a patient leaving the hospital without paying
+    no_gp_office = nil, -- Whether advice was given about patients and doctors waiting for a GP's office
+    no_doctor_no_gp_office = nil, -- Whether advice was given about patients waiting for a doctor and a GP's office
+    last_patient_not_paying_month = nil, -- Whether advice was given about a patient leaving the hospital without paying
   }
 
   self.win_declined = false -- Has not yet declined the level win fax
